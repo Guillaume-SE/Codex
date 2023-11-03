e5818d212ef904132988675d1d5c1a83958bb194
Guillaume-SE <guillaume.seure-even@3wa.io>
2023-11-03 21:38:38 +0100

    change: add explicit method

--- app/Routes/MediasRoutes.ts
+++ app/Routes/MediasRoutes.ts
@@ -2,13 +2,13 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
   Route.get('/', 'MediasController.getAll').as('medias.index')
-  Route.get('/:id', 'MediasController.getById').as('medias.show')
-  // findBy
+  Route.get('/:id', 'MediasController.getOneById').as('medias.show')
+  // Route.get('/query', 'MediasController.getOneById')
 
   //ADMIN
   Route.post('/', 'MediasController.addOne').as('medias.store')
-  Route.put('/:id', 'MediasController.update').as('medias.update')
-  Route.delete('/:id', 'MediasController.destroy').as('medias.delete')
+  Route.put('/:id', 'MediasController.updateOne').as('medias.update')
+  Route.delete('/:id', 'MediasController.deleteOne').as('medias.delete')
 })
   .prefix('/medias')
   .prefix('/api')
