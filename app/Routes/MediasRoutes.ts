69bb0914afab96022189ce41ebe5d4375b5a9d79
Guillaume-SE <guillaume.seure-even@3wa.io>
2023-11-07 14:58:54 +0100

    fix: suppression et ajouts routes

--- app/Routes/MediasRoutes.ts
+++ app/Routes/MediasRoutes.ts
@@ -1,14 +1,16 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
-  Route.get('/', 'MediasController.getAll').as('medias.index')
-  Route.get('/:id', 'MediasController.getOneById').as('medias.show')
+  Route.get('/', 'MediasController.getAllMedias')
+  Route.get('/movies', 'MediasController.getAllMovies')
+  Route.get('/games', 'MediasController.getAllGames')
+  Route.get('/:id', 'MediasController.getOneMediaById')
   // Route.get('/query', 'MediasController.getOneById')
 
   //ADMIN
-  Route.post('/', 'MediasController.addOne').as('medias.store')
-  Route.put('/:id', 'MediasController.updateOne').as('medias.update')
-  Route.delete('/:id', 'MediasController.deleteOne').as('medias.delete')
+  Route.post('/', 'MediasController.addOneMedia')
+  Route.put('/:id', 'MediasController.updateOneMedia')
+  Route.delete('/:id', 'MediasController.deleteOneMedia')
 })
   .prefix('/medias')
   .prefix('/api')
