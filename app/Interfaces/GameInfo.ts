fc9f073eac69f1492b744f063e2ef3bbe1b76c39
Guillaume-SE <guillaume.seure-even@3wa.io>
2023-11-22 18:11:26 +0100

    deleted

--- app/Interfaces/GameInfo.ts
+++
@@ -1,9 +0,0 @@
-import { GamePlateform } from "App/Models/Enums/GamePlateform";
-
-export interface IGameInfo {
-    id?: number,
-    media_id: number,
-    developer: string,
-    publisher: string,
-    plateform: GamePlateform
-}
--- app/Interfaces/MovieInfo.ts
+++
@@ -1,7 +0,0 @@
-export interface IMovieInfo {
-    id?: number,
-    media_id: number,
-    director: string,
-    screenwriter: string,
-    duration: string
-}
