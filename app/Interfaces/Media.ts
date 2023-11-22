c895223944af4988b3b50f9aecfbe7b7ee9ee0f6
Guillaume-SE <guillaume.seure-even@3wa.io>
2023-11-22 18:11:52 +0100

    ajout interfaces

--- app/Interfaces/Media.ts
+++ app/Interfaces/Media.ts
@@ -1,12 +1,25 @@
 import { AttachmentContract } from "@ioc:Adonis/Addons/AttachmentLite";
+import { GamePlateform } from "App/Models/Enums/GamePlateform";
 import { MediaTypes } from "App/Models/Enums/MediaTypes";
 
 export interface IMedia {
     id?: number
-    media_parent_id: number | null,
+    mediaParentId: number | null,
     name: string,
     type: MediaTypes,
     cover: AttachmentContract | null,
     released: string
     synopsis: string
+}
+
+export interface IGame extends IMedia {
+    developer: string,
+    publisher: string,
+    plateform: GamePlateform
+}
+
+export interface IMovie extends IMedia {
+    director: number,
+    screenwriter: string,
+    duration: string
 }
