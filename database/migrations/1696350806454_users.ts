7f9d5f5980a81bebbacc16abdea1eeb2a30b2b9c
Guillaume-SE <guillaume.seure-even@3wa.io>
2023-11-30 21:57:45 +0100

    change place username

--- database/migrations/1696350806454_users.ts
+++ database/migrations/1696350806454_users.ts
@@ -6,9 +6,9 @@ export default class extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+      table.string('username', 30).notNullable()
       table.string('email').unique().notNullable()
       table.string('password').notNullable()
-      table.string('username', 30).notNullable()
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
     })
