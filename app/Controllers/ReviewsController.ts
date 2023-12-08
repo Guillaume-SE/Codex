6227779cf9b073ea359bc8a6bc4790fdaafb0581
Guillaume-SE <guillaume.seure-even@3wa.io>
2023-12-08 21:19:07 +0100

    add explicit name

--- app/Controllers/ReviewsController.ts
+++ app/Controllers/ReviewsController.ts
@@ -11,11 +11,11 @@ export default class ReviewsController {
   }
 
   public async addOneReview({ request, response, params }: HttpContextContract) {
-    const reviewStatus = validReviewStatus
+    const allValidReviewStatus = validReviewStatus
     const createdBy = 1
     const mediaId = parseInt(params.mediaId)
     const { status, rating, notes, isFavorite } = request.body()
-    const asNoValidStatus = !reviewStatus.includes(status)
+    const asNoValidStatus = !allValidReviewStatus.includes(status)
 
     if (asNoValidStatus) {
       return response.status(404).json({ message: "Le statut de la review n'est pas valide" })
@@ -41,8 +41,15 @@ export default class ReviewsController {
   }
 
   public async updateOneReview({ request, params, response }: HttpContextContract) {
+    const allValidReviewStatus = validReviewStatus
     const mediaId = params.mediaId
     const updatedReview = request.body()
+    const asNoValidStatus = !allValidReviewStatus.includes(updatedReview.status)
+
+    if (asNoValidStatus) {
+      return response.status(404).json({ message: "Le statut de la review n'est pas valide" })
+    }
+
     const media = await Media.find(mediaId)
     if (!media) {
       return response.status(404).json("Aucun media n'a été trouvé")
