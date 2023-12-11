02297a2d7fb544d0518f3134b67b05bbf491d8bf
Guillaume-SE <guillaume.seure-even@3wa.io>
2023-12-11 16:37:04 +0100

    add specific types

--- app/Models/Enums/MediaTypes.ts
+++ app/Models/Enums/MediaTypes.ts
@@ -1,16 +1,25 @@
 const types = [
-    'jeu vidéo',
-    'dlc',
-    'série',
-    'animé',
-    'dessin animé',
-    'cartoon',
-    'film',
-    'manga',
-    'roman',
-    'comics',
-    'bande dessinée',
-    'artbook'
+  'jeu vidéo',
+  'dlc',
+  'série',
+  'animé',
+  'dessin animé',
+  'cartoon',
+  'film',
+  'manga',
+  'roman',
+  'comics',
+  'bande dessinée',
+  'artbook',
 ] as const
 
-export type MediaTypes = typeof types[number]
+export type MediaTypes = (typeof types)[number]
+export { types as validMediaTypes }
+
+export const gameTypes = ['jeu vidéo', 'dlc']
+
+export const movieTypes = ['film']
+
+export const seasonTypes = ['série', 'animé', 'dessin animé', 'cartoon']
+
+export const bookTypes = ['manga', 'roman', 'comics', 'bande dessinée', 'artbook']
