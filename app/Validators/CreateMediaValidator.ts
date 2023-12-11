a3543af2c7b0b4c40f8832f50642216642f46263
Guillaume-SE <guillaume.seure-even@3wa.io>
2023-12-11 16:36:34 +0100

    change: string to enum

--- app/Validators/CreateMediaValidator.ts
+++ app/Validators/CreateMediaValidator.ts
@@ -1,22 +1,24 @@
 import { schema, CustomMessages, rules } from '@ioc:Adonis/Core/Validator'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { validMediaTypes } from 'App/Models/Enums/MediaTypes'
+import { validGamePlatform } from 'App/Models/Enums/GamePlatform'
 
 export default class CreateMediaValidator {
   constructor(protected ctx: HttpContextContract) {}
 
   public schema = schema.create({
     mediaParentId: schema.number.nullable(),
-    type: schema.string([rules.trim()]),
+    type: schema.enum(validMediaTypes, [rules.trim()]),
     cover: schema.file.nullable(),
     name: schema.string([rules.trim()]),
     released: schema.string([rules.trim()]),
     synopsis: schema.string([rules.trim()]),
     developer: schema.string.optional([rules.trim()]),
     publisher: schema.string.optional([rules.trim()]),
-    plateform: schema.string.optional([rules.trim()]),
+    plateform: schema.enum.optional(validGamePlatform, [rules.trim()]),
     director: schema.string.optional([rules.trim()]),
     screenwriter: schema.string.optional([rules.trim()]),
-    duration: schema.number.optional([rules.unsigned()])
+    duration: schema.number.optional([rules.unsigned()]),
   })
 
   public messages: CustomMessages = {}
