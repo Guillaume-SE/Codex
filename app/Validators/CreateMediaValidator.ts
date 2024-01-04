d657d6b2d9bc8ff0020be0949dca65bba8b3e160
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-01-04 16:42:00 +0100

    refacto + add boook and season infos

--- app/Validators/CreateMediaValidator.ts
+++ app/Validators/CreateMediaValidator.ts
@@ -1,7 +1,7 @@
 import { schema, CustomMessages, rules } from '@ioc:Adonis/Core/Validator'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import { validMediaTypes } from 'App/Models/Enums/MediaTypes'
-import { validGamePlatform } from 'App/Models/Enums/GamePlatform'
+import { validMediaTypes } from 'App/Tools/Enums/MediaTypes'
+import { validGamePlatform } from 'App/Tools/Enums/GamePlatform'
 
 export default class CreateMediaValidator {
   constructor(protected ctx: HttpContextContract) {}
@@ -16,12 +16,22 @@ export default class CreateMediaValidator {
     name: schema.string([rules.trim()]),
     released: schema.string([rules.trim()]),
     synopsis: schema.string([rules.trim()]),
+    // for games type
     developer: schema.string.optional([rules.trim()]),
     publisher: schema.string.optional([rules.trim()]),
     plateform: schema.enum.optional(validGamePlatform, [rules.trim()]),
+    // for movies type
     director: schema.string.optional([rules.trim()]),
     screenwriter: schema.string.optional([rules.trim()]),
     duration: schema.number.optional([rules.unsigned()]),
+    // for books type
+    author: schema.string.optional([rules.trim()]),
+    illustrator: schema.string.optional([rules.trim()]),
+    editor: schema.string.optional([rules.trim()]),
+    pages: schema.number.optional([rules.unsigned()]),
+    // for seasons type
+    creator: schema.string.optional([rules.trim()]),
+    length: schema.number.optional([rules.unsigned()]),
   })
 
   public messages: CustomMessages = {}
