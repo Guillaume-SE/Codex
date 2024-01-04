cd6723df5ed6ffbf1dfc9a851e8162a74ff432f2
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-01-04 16:41:44 +0100

    moved to Tools folder

---
+++ app/Tools/Interfaces/Media.ts
@@ -0,0 +1,39 @@
+import { GamePlateform } from "App/Tools/Enums/GamePlatform";
+import { MediaTypes } from "App/Tools/Enums/MediaTypes";
+import { DateTime } from "luxon";
+
+export interface IMedia {
+    id: number
+    mediaParentId: number | null,
+    name: string,
+    type: MediaTypes,
+    cover: string | null,
+    released: string,
+    synopsis: string,
+    createdAt?: DateTime,
+    updatedAt?: DateTime
+}
+
+export interface IGame extends IMedia {
+    developer: string,
+    publisher: string,
+    plateform: GamePlateform
+}
+
+export interface IMovie extends IMedia {
+    director: string,
+    screenwriter: string,
+    duration: number
+}
+
+export interface IBook extends IMedia {
+    author: string,
+    illustrator: string,
+    editor: string,
+    pages: number
+}
+
+export interface ISeason extends IMedia {
+    creator: string,
+    length: number
+}
