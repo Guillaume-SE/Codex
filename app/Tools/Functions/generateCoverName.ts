68428aaddff03ea9b2ffd086e5f7d270cdc1160d
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-01-23 21:07:44 +0100

    get rid of extname arguments

--- app/Tools/Functions/generateCoverName.ts
+++ app/Tools/Functions/generateCoverName.ts
@@ -2,7 +2,7 @@ import { string } from '@ioc:Adonis/Core/Helpers'
 // import { ValidFileExtension } from 'App/Tools/Enums/FileExtension'
 
 
-export function createFileName(extname: string = 'jpg'): string {
+export function createFileName(): string {
   const timestamp = new Date().getTime().toString()
   const randomString = string.generateRandom(10)
   const fullName = `${timestamp}-${randomString}.jpg`
