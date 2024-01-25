4d57f41a4cb4888ae235c5a5b308e705d540d110
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-01-25 12:11:25 +0100

    removed email column

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -1,37 +1,26 @@
 import { DateTime } from 'luxon'
-import Hash from '@ioc:Adonis/Core/Hash'
-import { BaseModel, HasMany, beforeSave, column, hasMany } from '@ioc:Adonis/Lucid/Orm'
-import Review from 'App/Models/Review'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
 
 export default class User extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
   @column()
-  public email: string
+  public username: string
 
   @column({ serializeAs: null })
   public password: string
 
-  @column()
-  public username: string
-
   @column.dateTime({ autoCreate: true, columnName: 'created_at' })
   public createdAt: DateTime
 
-  @column.dateTime({ autoCreate: true, autoUpdate: true, columnName: 'updated_at'  })
+  @column.dateTime({ autoCreate: true, autoUpdate: true, columnName: 'updated_at' })
   public updatedAt: DateTime
-
-  @beforeSave()
-  public static async hashPassword(user: User) {
-    if (user.$dirty.password) {
-      user.password = await Hash.make(user.password)
-    }
-  }
-
-  // RELATIONS
-  @hasMany(() => Review, {
-    foreignKey: 'createdBy'
-  })
-  public reviews: HasMany<typeof Review>
 }
+
+// @beforeSave()
+// public static async hashPassword(user: User) {
+//   if (user.$dirty.password) {
+//     user.password = await Hash.make(user.password)
+//   }
+// }
--- database/migrations/1696350806454_users.ts
+++ database/migrations/1696350806454_users.ts
@@ -7,7 +7,6 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
       table.string('username', 30).notNullable()
-      table.string('email').unique().notNullable()
       table.string('password').notNullable()
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
