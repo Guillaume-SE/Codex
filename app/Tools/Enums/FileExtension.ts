0d4235a19d78b880bbd4b9bf9888d5a2fa06cd1c
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-01-25 16:26:02 +0100

    minnor cleaning

--- app/Tools/Enums/FileExtension.ts
+++ app/Tools/Enums/FileExtension.ts
@@ -1,9 +1,5 @@
-const extension = [
-    'png',
-    'jpg',
-    'jpeg',
-    'webp'
-] as const
+export const extension = ['png', 'jpg', 'jpeg', 'webp'] as const
 
-export type FileExtension = typeof extension[number]
-export { extension as validFileExtension }
+export type FileExtension = (typeof extension)[number]
+
+export const validCoverFileExtension = ['png', 'webp', 'jpg', 'jpeg']
