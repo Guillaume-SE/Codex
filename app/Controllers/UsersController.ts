1651b8454db6bfbb65d46b49891601d6aa21cdc8
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-01-25 12:12:48 +0100

    removed update users routes

--- app/Controllers/UsersController.ts
+++ app/Controllers/UsersController.ts
@@ -4,9 +4,4 @@ export default class UsersController {
   public async show({ params }: HttpContextContract) {
     return `Return the user with id ${params.id}`
   }
-
-  //ADMIN
-  public async update({ params }: HttpContextContract) {
-    return `Updated the game with id ${params.id}`
-  }
 }
--- app/Routes/UsersRoutes.ts
+++ app/Routes/UsersRoutes.ts
@@ -2,8 +2,6 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
   Route.get('/:id', 'UsersController.show').as('users.show')
-  //ADMIN
-  Route.put('/:id', 'UsersController.update').as('users.update')
 })
   .prefix('/users')
   .prefix('/api')
