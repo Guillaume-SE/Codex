a7d7233917dbf509136c79885adee864bd36c130
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-01-29 11:14:28 +0100

    add delete route for cover

--- app/Routes/CoversRoutes.ts
+++ app/Routes/CoversRoutes.ts
@@ -3,6 +3,7 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.group(() => {
   Route.get('/', 'CoversController.getAllCovers')
   Route.put('/:mediaId', 'CoversController.updateOneCover')
+  Route.delete('/:mediaId', 'CoversController.deleteOneCover')
 })
   .prefix('/covers')
   .prefix('/api')
