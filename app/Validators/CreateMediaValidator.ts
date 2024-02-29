232b6a9bd3339002a43439056b37825e0687099a
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-02-29 16:29:57 +0100

    add review validation

--- app/Validators/CreateMediaValidator.ts
+++ app/Validators/CreateMediaValidator.ts
@@ -3,6 +3,7 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { validMediaTypes } from 'App/Tools/Enums/MediaTypes'
 import { validGamePlatform } from 'App/Tools/Enums/GamePlatform'
 import { validCoverFileExtension } from 'App/Tools/Enums/FileExtension'
+import { validReviewStatus } from 'App/Tools/Enums/ReviewStatus'
 
 export default class CreateMediaValidator {
   constructor(protected ctx: HttpContextContract) {}
@@ -33,6 +34,11 @@ export default class CreateMediaValidator {
     // for seasons type
     creator: schema.string.optional([rules.trim()]),
     length: schema.number.optional([rules.unsigned()]),
+    //for review
+    status: schema.enum(validReviewStatus, [rules.trim()]),
+    rating: schema.number.nullable([rules.range(0, 10)]),
+    opinion: schema.string.nullable([rules.trim()]),
+    isFavorite: schema.boolean(),
   })
 
   public messages: CustomMessages = {}
