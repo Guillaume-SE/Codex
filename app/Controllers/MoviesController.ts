275df729ae4b2ef0e2ad55e9cba9f51bf20e92e2
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-03-05 19:11:37 +0100

    moved create & update from media controller

--- app/Controllers/MoviesController.ts
+++ app/Controllers/MoviesController.ts
@@ -1,7 +1,122 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Database from '@ioc:Adonis/Lucid/Database'
+import Drive from '@ioc:Adonis/Core/Drive'
 import Media from 'App/Models/Media'
+import { movieTypes } from 'App/Tools/Enums/MediaTypes'
+import { createFileName, defaultCoverFilename } from 'App/Tools/Functions/generateCoverName'
+import {
+  createAlternativeText,
+  defaultCoverAltText,
+} from 'App/Tools/Functions/generateCoverAltText'
+import { standardize } from 'App/Tools/Functions/standardizeCover'
+import CreateMovieValidator from 'App/Validators/CreateMovieValidator'
+import UpdateMovieValidator from 'App/Validators/UpdateMovieValidator'
 
 export default class MoviesController {
+  public async addOneMovie({ request, response }: HttpContextContract) {
+    const payloadValidation = await request.validate(CreateMovieValidator)
+    const {
+      mediaParentId,
+      type,
+      cover,
+      name,
+      released,
+      synopsis,
+      status,
+      rating,
+      opinion,
+      isFavorite,
+      ...specificMovieInfos
+    } = payloadValidation
+
+    const searchIfMovieAlreadyExist = await Media.query()
+      .from('medias')
+      .where('type', type)
+      .andWhere('name', name)
+      .andWhere('released', released)
+    const movieAlreadyExist = searchIfMovieAlreadyExist.length > 0
+
+    if (movieAlreadyExist) {
+      return response.status(400).json({
+        message: 'Ce film a déjà été ajouté !',
+        media: searchIfMovieAlreadyExist,
+      })
+    }
+    const mediaIsMovieType = movieTypes.includes(type)
+    if (!mediaIsMovieType) {
+      return response.status(400).json({
+        message: 'Le type d media ne correspond pas à la catégorie film',
+      })
+    }
+
+    // manage covers
+    let coverName: string = defaultCoverFilename
+    let coverAltText: string = defaultCoverAltText
+    if (cover) {
+      coverName = createFileName()
+      coverAltText = createAlternativeText(type, name)
+      const coverFormated = standardize(cover.tmpPath)
+      await Drive.put(`covers/${coverName}`, await coverFormated, {
+        contentType: `image/jpg`,
+      })
+    }
+
+    const generalMediaInfos = { mediaParentId, type, name, released, synopsis }
+    const coverInfo = { filename: coverName, alternative: coverAltText }
+    const reviewInfo = { status, rating, opinion, isFavorite }
+
+    const trx = await Database.transaction()
+    try {
+      const newMedia = await Media.create(generalMediaInfos)
+      await newMedia.related('movieInfo').create(specificMovieInfos)
+      await newMedia.related('cover').create(coverInfo)
+      await newMedia.related('review').create(reviewInfo)
+
+      await trx.commit()
+      return response.status(201).json({ newMedia, coverInfo, reviewInfo })
+    } catch (error) {
+      await trx.rollback()
+      if (coverName !== defaultCoverFilename) {
+        await Drive.delete(`covers/${coverName}`)
+      }
+      return response.status(400).json(error)
+    }
+  }
+
+  public async updateOneMovie({ request, params, response }: HttpContextContract) {
+    const mediaId = params.id
+    const mediaToUpdate = await Media.find(mediaId)
+    const mediaDoesntExist = !mediaToUpdate
+    if (mediaDoesntExist) {
+      return response.status(404).json("Le film à mettre à jour n'existe pas")
+    }
+
+    const payloadValidation = await request.validate(UpdateMovieValidator)
+    const { mediaParentId, type, name, released, synopsis, ...specificMovieInfos } =
+      payloadValidation
+    const generalMediaInfos = { mediaParentId, type, name, released, synopsis }
+
+    const mediaIsMovieType = movieTypes.includes(type)
+    if (!mediaIsMovieType) {
+      return response.status(400).json({
+        message: 'Le type ne correspond pas à la section film',
+      })
+    }
+
+    const trx = await Database.transaction()
+
+    try {
+      mediaToUpdate.merge(generalMediaInfos).save()
+      await mediaToUpdate.related('movieInfo').updateOrCreate({}, specificMovieInfos)
+
+      await trx.commit()
+      return response.status(201).json(mediaToUpdate)
+    } catch (error) {
+      await trx.rollback()
+      return response.status(400).json(error)
+    }
+  }
+
   public async getAllMoviesWithReviews({ response }: HttpContextContract) {
     const datas = await Media.query()
       .from('medias')
