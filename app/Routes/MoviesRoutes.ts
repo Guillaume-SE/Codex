c6a1ce0f3b07bc3d633206c48fdb112bde30ffee
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-03-05 19:12:22 +0100

    add create & update movie routes

--- app/Routes/MoviesRoutes.ts
+++ app/Routes/MoviesRoutes.ts
@@ -3,6 +3,10 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.group(() => {
   Route.get('/', 'MoviesController.getAllMoviesWithReviews')
   Route.get('/:mediaId', 'MoviesController.getOneMovieWithReviewByMediaId')
+
+  //ADMIN
+  Route.post('/', 'MoviesController.addOneMovie')
+  Route.put('/:id', 'MoviesController.updateOneMovie')
 })
   .prefix('/movies')
   .prefix('/api')
