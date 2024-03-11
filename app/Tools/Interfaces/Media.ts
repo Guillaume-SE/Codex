7aee11bae293080f457d5ff88b68297e6cf597d4
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-03-11 16:26:40 +0100

    fixed typo on platform word

--- app/Tools/Interfaces/Media.ts
+++ app/Tools/Interfaces/Media.ts
@@ -1,38 +1,38 @@
-import { GamePlateform } from "App/Tools/Enums/GamePlatform";
-import { MediaTypes } from "App/Tools/Enums/MediaTypes";
-import { DateTime } from "luxon";
+import { GamePlatform } from 'App/Tools/Enums/GamePlatform'
+import { MediaTypes } from 'App/Tools/Enums/MediaTypes'
+import { DateTime } from 'luxon'
 
 export interface IMedia {
-    id: number
-    mediaParentId: number | null,
-    type: MediaTypes,
-    name: string,
-    released: string,
-    synopsis: string,
-    createdAt?: DateTime,
-    updatedAt?: DateTime
+  id: number
+  mediaParentId: number | null
+  type: MediaTypes
+  name: string
+  released: string
+  synopsis: string
+  createdAt?: number
+  updatedAt?: number
 }
 
 export interface IGame extends IMedia {
-    developer: string,
-    publisher: string,
-    plateform: GamePlateform
+  developer: string
+  publisher: string
+  platform: GamePlatform
 }
 
 export interface IMovie extends IMedia {
-    director: string,
-    screenwriter: string,
-    duration: number
+  director: string
+  screenwriter: string
+  duration: number
 }
 
 export interface IBook extends IMedia {
-    author: string,
-    illustrator: string,
-    editor: string,
-    pages: number
+  author: string
+  illustrator: string
+  editor: string
+  pages: number
 }
 
 export interface ISeason extends IMedia {
-    creator: string,
-    length: number
+  creator: string
+  length: number
 }
