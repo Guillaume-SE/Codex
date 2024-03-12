5809ff682a33a4db51c5bed2724ba3b417b1d4a0
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-03-12 16:35:02 +0100

    moved rules to dedicated validator

--- app/Validators/CreateMediaValidator.ts
+++ app/Validators/CreateMediaValidator.ts
@@ -9,23 +9,6 @@ export default class CreateMediaValidator {
   constructor(protected ctx: HttpContextContract) {}
 
   public schema = schema.create({
-    mediaParentId: schema.number.nullable(),
-    type: schema.enum(validMediaTypes, [rules.trim()]),
-    cover: schema.file.nullable({
-      size: '2mb',
-      extnames: validCoverFileExtension,
-    }),
-    name: schema.string([rules.trim()]),
-    released: schema.string([rules.trim()]),
-    synopsis: schema.string([rules.trim()]),
-    // for games type
-    developer: schema.string.optional([rules.trim()]),
-    publisher: schema.string.optional([rules.trim()]),
-    plateform: schema.enum.optional(validGamePlatform, [rules.trim()]),
-    // for movies type
-    director: schema.string.optional([rules.trim()]),
-    screenwriter: schema.string.optional([rules.trim()]),
-    duration: schema.number.optional([rules.unsigned()]),
     // for books type
     author: schema.string.optional([rules.trim()]),
     illustrator: schema.string.optional([rules.trim()]),
