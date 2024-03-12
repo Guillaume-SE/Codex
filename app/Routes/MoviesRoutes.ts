ae055054c082e001904714e82d8d080150d9f548
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-03-12 16:36:41 +0100

    add update and get routes

--- app/Routes/MoviesRoutes.ts
+++ app/Routes/MoviesRoutes.ts
@@ -1,8 +1,8 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
-  Route.get('/', 'MoviesController.getAllMoviesWithReviews')
-  Route.get('/:mediaId', 'MoviesController.getOneMovieWithReviewByMediaId')
+  Route.get('/', 'MoviesController.getAllMovies')
+  Route.get('/:mediaId', 'MoviesController.getOneMovieByMediaId')
 
   //ADMIN
   Route.post('/', 'MoviesController.addOneMovie')
