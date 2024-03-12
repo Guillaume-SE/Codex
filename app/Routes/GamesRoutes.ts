68cc0a33de6f6ab2153fb2536ca3f47dc28c72b0
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-03-12 16:37:11 +0100

    add create and update routes

--- app/Routes/GamesRoutes.ts
+++ app/Routes/GamesRoutes.ts
@@ -1,8 +1,12 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
-  Route.get('/', 'GamesController.getAllGamesWithReviews')
-  Route.get('/:mediaId', 'MediasController.getOneMediaById')
+  Route.get('/', 'GamesController.getAllGames')
+  Route.get('/:mediaId', 'GamesController.getOneGameByMediaId')
+
+  //ADMIN
+  Route.post('/', 'GamesController.addOneGame')
+  Route.put('/:id', 'GamesController.updateOneGame')
 })
   .prefix('/games')
   .prefix('/api')
