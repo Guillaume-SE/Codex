07f716c0a2b3ff9773794119f9446946ad2d5684
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-03-22 15:26:16 +0100

    added seasons routes

--- app/Routes/HomeRoutes.ts
+++ app/Routes/HomeRoutes.ts
@@ -1,10 +1,11 @@
 import Route from '@ioc:Adonis/Core/Route'
-import 'App/Routes/UsersRoutes'
-import 'App/Routes/MediasRoutes'
+import 'App/Routes/CoversRoutes'
 import 'App/Routes/GamesRoutes'
+import 'App/Routes/MediasRoutes'
 import 'App/Routes/MoviesRoutes'
 import 'App/Routes/ReviewsRoutes'
-import 'App/Routes/CoversRoutes'
+import 'App/Routes/SeasonsRoutes'
+import 'App/Routes/UsersRoutes'
 
 Route.get('/', async () => {
   return 'Home'
