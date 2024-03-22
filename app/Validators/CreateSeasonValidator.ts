dc00035cdb34f6483f854eb52d7c5360baf5ce89
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-03-22 15:27:09 +0100

    added validator for creating a Season

---
+++ app/Validators/CreateSeasonValidator.ts
@@ -0,0 +1,30 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { CustomMessages, rules, schema } from '@ioc:Adonis/Core/Validator'
+import { validCoverFileExtension } from 'App/Tools/Enums/FileExtension'
+import { validMediaTypes } from 'App/Tools/Enums/MediaTypes'
+import { validReviewStatus } from 'App/Tools/Enums/ReviewStatus'
+
+export default class CreateSeasonValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  public schema = schema.create({
+    mediaParentId: schema.number.nullable(),
+    type: schema.enum(validMediaTypes, [rules.trim()]),
+    cover: schema.file.nullable({
+      size: '2mb',
+      extnames: validCoverFileExtension,
+    }),
+    name: schema.string([rules.trim()]),
+    released: schema.string([rules.trim()]),
+    synopsis: schema.string([rules.trim()]),
+    creator: schema.string([rules.trim()]),
+    length: schema.number([rules.unsigned()]),
+    //review
+    status: schema.enum(validReviewStatus, [rules.trim()]),
+    rating: schema.number.nullable([rules.range(0, 10)]),
+    opinion: schema.string.nullable([rules.trim()]),
+    isFavorite: schema.boolean(),
+  })
+
+  public messages: CustomMessages = {}
+}
