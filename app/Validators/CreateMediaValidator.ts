ff25999bf4529431532c09970749908a59815f22
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-03-22 15:26:41 +0100

    moved season validation to SeasonValidator

--- app/Validators/CreateMediaValidator.ts
+++ app/Validators/CreateMediaValidator.ts
@@ -1,9 +1,5 @@
-import { schema, CustomMessages, rules } from '@ioc:Adonis/Core/Validator'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import { validMediaTypes } from 'App/Tools/Enums/MediaTypes'
-import { validGamePlatform } from 'App/Tools/Enums/GamePlatform'
-import { validCoverFileExtension } from 'App/Tools/Enums/FileExtension'
-import { validReviewStatus } from 'App/Tools/Enums/ReviewStatus'
+import { CustomMessages, rules, schema } from '@ioc:Adonis/Core/Validator'
 
 export default class CreateMediaValidator {
   constructor(protected ctx: HttpContextContract) {}
@@ -14,14 +10,6 @@ export default class CreateMediaValidator {
     illustrator: schema.string.optional([rules.trim()]),
     editor: schema.string.optional([rules.trim()]),
     pages: schema.number.optional([rules.unsigned()]),
-    // for seasons type
-    creator: schema.string.optional([rules.trim()]),
-    length: schema.number.optional([rules.unsigned()]),
-    //for review
-    status: schema.enum(validReviewStatus, [rules.trim()]),
-    rating: schema.number.nullable([rules.range(0, 10)]),
-    opinion: schema.string.nullable([rules.trim()]),
-    isFavorite: schema.boolean(),
   })
 
   public messages: CustomMessages = {}
