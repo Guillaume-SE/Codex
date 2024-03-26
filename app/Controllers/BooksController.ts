2aa5a31d5bcbebaa82d8e087399e368aed9dd759
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-03-26 16:10:27 +0100

    now check if Book exist for update

--- app/Controllers/BooksController.ts
+++ app/Controllers/BooksController.ts
@@ -1,17 +1,18 @@
-import Drive from '@ioc:Adonis/Core/Drive'
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import Database from '@ioc:Adonis/Lucid/Database'
-import Cover from 'App/Models/Cover'
-import Media from 'App/Models/Media'
-import { bookTypes } from 'App/Tools/Enums/MediaTypes'
+import Drive from "@ioc:Adonis/Core/Drive"
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext"
+import Database from "@ioc:Adonis/Lucid/Database"
+import BookInfo from "App/Models/BookInfo"
+import Cover from "App/Models/Cover"
+import Media from "App/Models/Media"
+import { bookTypes } from "App/Tools/Enums/MediaTypes"
 import {
   createAlternativeText,
   defaultCoverAltText,
-} from 'App/Tools/Functions/generateCoverAltText'
-import { createFileName, defaultCoverFilename } from 'App/Tools/Functions/generateCoverName'
-import { standardize } from 'App/Tools/Functions/standardizeCover'
-import CreateBookValidator from 'App/Validators/CreateBookValidator'
-import UpdateBookValidator from 'App/Validators/UpdateBookValidator'
+} from "App/Tools/Functions/generateCoverAltText"
+import { createFileName, defaultCoverFilename } from "App/Tools/Functions/generateCoverName"
+import { standardize } from "App/Tools/Functions/standardizeCover"
+import CreateBookValidator from "App/Validators/CreateBookValidator"
+import UpdateBookValidator from "App/Validators/UpdateBookValidator"
 
 export default class BooksController {
   public async addOneBook({ request, response }: HttpContextContract) {
@@ -31,22 +32,22 @@ export default class BooksController {
     } = payloadValidation
 
     const searchIfBookAlreadyExist = await Media.query()
-      .from('medias')
-      .where('type', type)
-      .andWhere('name', name)
-      .andWhere('released', released)
+      .from("medias")
+      .where("type", type)
+      .andWhere("name", name)
+      .andWhere("released", released)
     const bookAlreadyExist = searchIfBookAlreadyExist.length > 0
 
     if (bookAlreadyExist) {
       return response.status(400).json({
-        message: 'Ce livre a déjà été ajouté !',
+        message: "Ce livre a déjà été ajouté !",
         media: searchIfBookAlreadyExist,
       })
     }
     const mediaIsBookType = bookTypes.includes(type)
     if (!mediaIsBookType) {
       return response.status(400).json({
-        message: 'Le type du media ne correspond pas à la catégorie livre',
+        message: "Le type du media ne correspond pas à la catégorie livre",
       })
     }
 
@@ -69,9 +70,9 @@ export default class BooksController {
     const trx = await Database.transaction()
     try {
       const newMedia = await Media.create(generalMediaInfos)
-      await newMedia.related('bookInfo').create(specificBookInfos)
-      await newMedia.related('cover').create(coverInfo)
-      await newMedia.related('review').create(reviewInfo)
+      await newMedia.related("bookInfo").create(specificBookInfos)
+      await newMedia.related("cover").create(coverInfo)
+      await newMedia.related("review").create(reviewInfo)
 
       await trx.commit()
       return response.status(201).json({ newMedia, coverInfo, reviewInfo })
@@ -89,13 +90,19 @@ export default class BooksController {
     const mediaToUpdate = await Media.find(mediaId)
     const mediaDoesntExist = !mediaToUpdate
     if (mediaDoesntExist) {
-      return response.status(404).json('Aucun résultat pour cet identifiant')
+      return response.status(404).json("Aucun media ne correspond à cet identifiant")
     }
 
-    const actualCover = await Cover.findBy('media_id', mediaId)
+    const actualCover = await Cover.findBy("media_id", mediaId)
     const coverDoesntExist = !actualCover
     if (coverDoesntExist) {
       return response.status(404).json("Aucune cover liée à ce media n'a été trouvée")
+    }
+
+    const bookToUpdate = await BookInfo.findBy("mediaId", mediaId)
+    const bookDoesntExist = !bookToUpdate
+    if (bookDoesntExist) {
+      return response.status(404).json("Aucun livre ne correspond à ce media")
     }
 
     const payloadValidation = await request.validate(UpdateBookValidator)
@@ -106,25 +113,27 @@ export default class BooksController {
     const mediaBookType = bookTypes.includes(type)
     if (!mediaBookType) {
       return response.status(400).json({
-        message: 'Le type ne correspond pas à la catégorie livre',
+        message: "Le type ne correspond pas à la catégorie livre",
       })
     }
 
     // update cover alt text
     const mediaNameAsChanged = mediaToUpdate.name !== payloadValidation.name
     const newCoverAltText = createAlternativeText(type, name)
-    if (mediaNameAsChanged) {
-      actualCover.merge({ alternative: newCoverAltText }).save()
-    }
 
     const trx = await Database.transaction()
 
     try {
       mediaToUpdate.merge(generalMediaInfos).save()
-      await mediaToUpdate.related('bookInfo').updateOrCreate({}, specificBookInfos)
+      bookToUpdate.merge(specificBookInfos).save()
+      if (mediaNameAsChanged) {
+        actualCover.merge({ alternative: newCoverAltText }).save()
+      }
 
       await trx.commit()
-      return response.status(201).json(mediaToUpdate)
+      return response
+        .status(201)
+        .json({ media: mediaToUpdate, bookInfos: bookToUpdate, cover: actualCover })
     } catch (error) {
       await trx.rollback()
       return response.status(400).json(error)
