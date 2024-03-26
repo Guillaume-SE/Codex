9998156c50261bb5a02a40698321f3b54524e208
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-03-26 16:11:40 +0100

    now check if Movie exist before update

--- app/Controllers/MoviesController.ts
+++ app/Controllers/MoviesController.ts
@@ -1,17 +1,18 @@
-import Drive from '@ioc:Adonis/Core/Drive'
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import Database from '@ioc:Adonis/Lucid/Database'
-import Cover from 'App/Models/Cover'
-import Media from 'App/Models/Media'
-import { movieTypes } from 'App/Tools/Enums/MediaTypes'
+import Drive from "@ioc:Adonis/Core/Drive"
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext"
+import Database from "@ioc:Adonis/Lucid/Database"
+import Cover from "App/Models/Cover"
+import Media from "App/Models/Media"
+import MovieInfo from "App/Models/MovieInfo"
+import { movieTypes } from "App/Tools/Enums/MediaTypes"
 import {
   createAlternativeText,
   defaultCoverAltText,
-} from 'App/Tools/Functions/generateCoverAltText'
-import { createFileName, defaultCoverFilename } from 'App/Tools/Functions/generateCoverName'
-import { standardize } from 'App/Tools/Functions/standardizeCover'
-import CreateMovieValidator from 'App/Validators/CreateMovieValidator'
-import UpdateMovieValidator from 'App/Validators/UpdateMovieValidator'
+} from "App/Tools/Functions/generateCoverAltText"
+import { createFileName, defaultCoverFilename } from "App/Tools/Functions/generateCoverName"
+import { standardize } from "App/Tools/Functions/standardizeCover"
+import CreateMovieValidator from "App/Validators/CreateMovieValidator"
+import UpdateMovieValidator from "App/Validators/UpdateMovieValidator"
 
 export default class MoviesController {
   public async addOneMovie({ request, response }: HttpContextContract) {
@@ -31,22 +32,22 @@ export default class MoviesController {
     } = payloadValidation
 
     const searchIfMovieAlreadyExist = await Media.query()
-      .from('medias')
-      .where('type', type)
-      .andWhere('name', name)
-      .andWhere('released', released)
+      .from("medias")
+      .where("type", type)
+      .andWhere("name", name)
+      .andWhere("released", released)
     const movieAlreadyExist = searchIfMovieAlreadyExist.length > 0
 
     if (movieAlreadyExist) {
       return response.status(400).json({
-        message: 'Ce film a déjà été ajouté !',
+        message: "Ce film a déjà été ajouté !",
         media: searchIfMovieAlreadyExist,
       })
     }
     const mediaIsMovieType = movieTypes.includes(type)
     if (!mediaIsMovieType) {
       return response.status(400).json({
-        message: 'Le type du media ne correspond pas à la catégorie film',
+        message: "Le type du media ne correspond pas à la catégorie film",
       })
     }
 
@@ -69,9 +70,9 @@ export default class MoviesController {
     const trx = await Database.transaction()
     try {
       const newMedia = await Media.create(generalMediaInfos)
-      await newMedia.related('movieInfo').create(specificMovieInfos)
-      await newMedia.related('cover').create(coverInfo)
-      await newMedia.related('review').create(reviewInfo)
+      await newMedia.related("movieInfo").create(specificMovieInfos)
+      await newMedia.related("cover").create(coverInfo)
+      await newMedia.related("review").create(reviewInfo)
 
       await trx.commit()
       return response.status(201).json({ newMedia, coverInfo, reviewInfo })
@@ -89,13 +90,19 @@ export default class MoviesController {
     const mediaToUpdate = await Media.find(mediaId)
     const mediaDoesntExist = !mediaToUpdate
     if (mediaDoesntExist) {
-      return response.status(404).json('Aucun résultat pour cet identifiant')
+      return response.status(404).json("Aucun media ne correspond à cet identifiant")
     }
 
-    const actualCover = await Cover.findBy('media_id', mediaId)
+    const actualCover = await Cover.findBy("media_id", mediaId)
     const coverDoesntExist = !actualCover
     if (coverDoesntExist) {
       return response.status(404).json("Aucune cover liée à ce media n'a été trouvée")
+    }
+
+    const movieToUpdate = await MovieInfo.findBy("mediaId", mediaId)
+    const movieDoesntExist = !movieToUpdate
+    if (movieDoesntExist) {
+      return response.status(404).json("Aucun film ne correspond à ce media")
     }
 
     const payloadValidation = await request.validate(UpdateMovieValidator)
@@ -106,25 +113,27 @@ export default class MoviesController {
     const mediaIsMovieType = movieTypes.includes(type)
     if (!mediaIsMovieType) {
       return response.status(400).json({
-        message: 'Le type ne correspond pas à la catégorie film',
+        message: "Le type ne correspond pas à la catégorie film",
       })
     }
 
     // update cover alt text
     const mediaNameAsChanged = mediaToUpdate.name !== payloadValidation.name
     const newCoverAltText = createAlternativeText(type, name)
-    if (mediaNameAsChanged) {
-      actualCover.merge({ alternative: newCoverAltText }).save()
-    }
 
     const trx = await Database.transaction()
 
     try {
       mediaToUpdate.merge(generalMediaInfos).save()
-      await mediaToUpdate.related('movieInfo').updateOrCreate({}, specificMovieInfos)
+      movieToUpdate.merge(specificMovieInfos).save()
+      if (mediaNameAsChanged) {
+        actualCover.merge({ alternative: newCoverAltText }).save()
+      }
 
       await trx.commit()
-      return response.status(201).json(mediaToUpdate)
+      return response
+        .status(201)
+        .json({ media: mediaToUpdate, movieInfos: movieToUpdate, cover: actualCover })
     } catch (error) {
       await trx.rollback()
       return response.status(400).json(error)
@@ -133,28 +142,28 @@ export default class MoviesController {
 
   public async getAllMovies({ response }: HttpContextContract) {
     const datas = await Media.query()
-      .from('medias')
-      .join('movies_infos', 'medias.id', '=', 'movies_infos.media_id')
-      .join('reviews', 'medias.id', '=', 'reviews.media_id')
-      .join('covers', 'medias.id', '=', 'covers.media_id')
+      .from("medias")
+      .join("movies_infos", "medias.id", "=", "movies_infos.media_id")
+      .join("reviews", "medias.id", "=", "reviews.media_id")
+      .join("covers", "medias.id", "=", "covers.media_id")
       .select(
-        'medias.id',
-        'medias.media_parent_id',
-        'medias.name',
-        'medias.type',
-        'medias.released',
-        'medias.synopsis',
-        'covers.filename',
-        'covers.alternative',
-        'movies_infos.director',
-        'movies_infos.screenwriter',
-        'movies_infos.duration',
-        'reviews.status',
-        'reviews.rating',
-        'reviews.opinion',
-        'reviews.is_favorite',
-        'reviews.created_at',
-        'reviews.updated_at'
+        "medias.id",
+        "medias.media_parent_id",
+        "medias.name",
+        "medias.type",
+        "medias.released",
+        "medias.synopsis",
+        "covers.filename",
+        "covers.alternative",
+        "movies_infos.director",
+        "movies_infos.screenwriter",
+        "movies_infos.duration",
+        "reviews.status",
+        "reviews.rating",
+        "reviews.opinion",
+        "reviews.is_favorite",
+        "reviews.created_at",
+        "reviews.updated_at"
       )
     const movies = datas.map((data) => {
       const { id, mediaParentId, name, type, released, synopsis } = data
@@ -204,30 +213,30 @@ export default class MoviesController {
   public async getOneMovieByMediaId({ params, response }: HttpContextContract) {
     const mediaId = params.mediaId
     const datas = await Media.query()
-      .from('medias')
-      .join('movies_infos', 'medias.id', '=', 'movies_infos.media_id')
-      .join('reviews', 'medias.id', '=', 'reviews.media_id')
-      .join('covers', 'medias.id', '=', 'covers.media_id')
+      .from("medias")
+      .join("movies_infos", "medias.id", "=", "movies_infos.media_id")
+      .join("reviews", "medias.id", "=", "reviews.media_id")
+      .join("covers", "medias.id", "=", "covers.media_id")
       .select(
-        'medias.id',
-        'medias.media_parent_id',
-        'medias.name',
-        'medias.type',
-        'medias.released',
-        'medias.synopsis',
-        'covers.filename',
-        'covers.alternative',
-        'movies_infos.director',
-        'movies_infos.screenwriter',
-        'movies_infos.duration',
-        'reviews.status',
-        'reviews.rating',
-        'reviews.opinion',
-        'reviews.is_favorite',
-        'reviews.created_at',
-        'reviews.updated_at'
+        "medias.id",
+        "medias.media_parent_id",
+        "medias.name",
+        "medias.type",
+        "medias.released",
+        "medias.synopsis",
+        "covers.filename",
+        "covers.alternative",
+        "movies_infos.director",
+        "movies_infos.screenwriter",
+        "movies_infos.duration",
+        "reviews.status",
+        "reviews.rating",
+        "reviews.opinion",
+        "reviews.is_favorite",
+        "reviews.created_at",
+        "reviews.updated_at"
       )
-      .where('medias.id', '=', mediaId)
+      .where("medias.id", "=", mediaId)
 
     const noMovieFound = datas.length === 0
     if (noMovieFound) {
