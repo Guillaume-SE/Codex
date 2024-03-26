51c98759bf71abfc804c2867b54883c1fa3a0836
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-03-26 16:28:13 +0100

    add getAllBooks and getBookByMediaId methods

--- app/Controllers/BooksController.ts
+++ app/Controllers/BooksController.ts
@@ -75,7 +75,7 @@ export default class BooksController {
       await newMedia.related("review").create(reviewInfo)
 
       await trx.commit()
-      return response.status(201).json({ newMedia, coverInfo, reviewInfo })
+      return response.status(201).json({ media: newMedia, cover: coverInfo, review: reviewInfo })
     } catch (error) {
       await trx.rollback()
       if (coverName !== defaultCoverFilename) {
@@ -138,5 +138,158 @@ export default class BooksController {
       await trx.rollback()
       return response.status(400).json(error)
     }
+  }
+
+  public async getAllBooks({ response }: HttpContextContract) {
+    const datas = await Media.query()
+      .from("medias")
+      .join("books_infos", "medias.id", "=", "books_infos.media_id")
+      .join("reviews", "medias.id", "=", "reviews.media_id")
+      .join("covers", "medias.id", "=", "covers.media_id")
+      .select(
+        "medias.id",
+        "medias.media_parent_id",
+        "medias.name",
+        "medias.type",
+        "medias.released",
+        "medias.synopsis",
+        "covers.filename",
+        "covers.alternative",
+        "books_infos.author",
+        "books_infos.illustrator",
+        "books_infos.editor",
+        "books_infos.pages",
+        "reviews.status",
+        "reviews.rating",
+        "reviews.opinion",
+        "reviews.is_favorite",
+        "reviews.created_at",
+        "reviews.updated_at"
+      )
+    const books = datas.map((data) => {
+      const { id, mediaParentId, name, type, released, synopsis } = data
+      const {
+        filename,
+        alternative,
+        author,
+        illustrator,
+        editor,
+        pages,
+        status,
+        rating,
+        opinion,
+        is_favorite: isFavorite,
+        created_at: createdAt,
+        updated_at: updatedAt,
+      } = data.$extras
+
+      return {
+        book: {
+          id,
+          mediaParentId,
+          type,
+          name,
+          released,
+          synopsis,
+          author,
+          illustrator,
+          editor,
+          pages,
+        },
+        cover: {
+          filename,
+          alternative,
+        },
+        review: {
+          status,
+          rating,
+          opinion,
+          isFavorite,
+          createdAt,
+          updatedAt,
+        },
+      }
+    })
+    return response.status(201).json(books)
+  }
+
+  public async getOneBookByMediaId({ params, response }: HttpContextContract) {
+    const mediaId = params.mediaId
+    const datas = await Media.query()
+      .from("medias")
+      .join("books_infos", "medias.id", "=", "books_infos.media_id")
+      .join("reviews", "medias.id", "=", "reviews.media_id")
+      .join("covers", "medias.id", "=", "covers.media_id")
+      .select(
+        "medias.id",
+        "medias.media_parent_id",
+        "medias.name",
+        "medias.type",
+        "medias.released",
+        "medias.synopsis",
+        "covers.filename",
+        "covers.alternative",
+        "books_infos.author",
+        "books_infos.illustrator",
+        "books_infos.editor",
+        "books_infos.pages",
+        "reviews.status",
+        "reviews.rating",
+        "reviews.opinion",
+        "reviews.is_favorite",
+        "reviews.created_at",
+        "reviews.updated_at"
+      )
+      .where("medias.id", "=", mediaId)
+
+    const noBookFound = datas.length === 0
+    if (noBookFound) {
+      return response.status(404).json("Aucun livre correspondant n'a été trouvé")
+    }
+    const movie = datas.map((data) => {
+      const { id, mediaParentId, name, type, released, synopsis } = data
+      const {
+        filename,
+        alternative,
+        author,
+        illustrator,
+        editor,
+        pages,
+        status,
+        rating,
+        opinion,
+        is_favorite: isFavorite,
+        created_at: createdAt,
+        updated_at: updatedAt,
+      } = data.$extras
+
+      return {
+        book: {
+          id,
+          mediaParentId,
+          name,
+          type,
+          released,
+          synopsis,
+          author,
+          illustrator,
+          editor,
+          pages,
+        },
+        cover: {
+          filename,
+          alternative,
+        },
+        review: {
+          status,
+          rating,
+          opinion,
+          isFavorite,
+          createdAt,
+          updatedAt,
+        },
+      }
+    })
+    return response.status(200).json(movie)
   }
 }
