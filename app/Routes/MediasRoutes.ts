39911bb1643122ebfb803150fe26e367193a6fde
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-03-27 22:20:31 +0100

    single quote to double quotes

--- app/Routes/MediasRoutes.ts
+++ app/Routes/MediasRoutes.ts
@@ -1,12 +1,11 @@
-import Route from '@ioc:Adonis/Core/Route'
+import Route from "@ioc:Adonis/Core/Route"
 
 Route.group(() => {
-  Route.get('/', 'MediasController.getAllMedias')
-  Route.get('/:id', 'MediasController.getOneMediaById')
-  // Route.get('/query', 'MediasController.getOneById')
+  Route.get("/", "MediasController.getAllMedias")
+  Route.get("/:id", "MediasController.getOneMediaById")
 
   //ADMIN
-  Route.delete('/:id', 'MediasController.deleteOneMedia')
+  Route.delete("/:id", "MediasController.deleteOneMedia")
 })
-  .prefix('/medias')
-  .prefix('/api')
+  .prefix("/medias")
+  .prefix("/api")
