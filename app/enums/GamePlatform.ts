75698780129278fd6975ba03644684294ab15348
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-04-17 11:05:02 +0200

    all valid game platform

---
+++ app/enums/GamePlatform.ts
@@ -0,0 +1,23 @@
+const platform = [
+  'Xbox',
+  'Xbox 360',
+  'Xbox One',
+  'Xbox Series X',
+  'PlayStation',
+  'PlayStation 2',
+  'PlayStation 3',
+  'PlayStation 4',
+  'PlayStation 5',
+  'PSP',
+  'PlayStation Vita',
+  'Sega Saturn',
+  'Game Boy Advance',
+  'Nintendo DS',
+  'Nintendo Switch',
+  'PC',
+  'Mobile',
+  'Emulateur',
+] as const
+
+export type GamePlatform = (typeof platform)[number]
+export { platform as validGamePlatform }
