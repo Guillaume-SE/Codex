88d9aed7237309504a157722de12742f5a743bfc
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-04-23 15:35:10 +0200

    feat: add interfaces for all types of Media

--- app/interfaces/media_interfaces.ts
+++ app/interfaces/media_interfaces.ts
@@ -1,1 +1,42 @@
-export interface Media {}
+import { GamePlatform } from '#enums/GamePlatform'
+import { MediaTypes } from '#enums/MediaTypes'
+import { ReviewStatus } from '#enums/ReviewStatus'
+import { MultipartFile } from '@adonisjs/core/bodyparser'
+
+export interface IMedia {
+  id: number
+  mediaParentId: number | null
+  cover?: MultipartFile
+  type: MediaTypes
+  name: string
+  released: string
+  synopsis: string
+  status?: ReviewStatus
+  rating?: number
+  opinion?: string
+  isFavorite?: boolean
+}
+
+export interface IBook extends IMedia {
+  author: string
+  illustrator: string | null
+  editor: string
+  pages: number
+}
+
+export interface IMovie extends IMedia {
+  director: string
+  screenwriter: string
+  duration: number
+}
+
+export interface IGame extends IMedia {
+  developer: string
+  publisher: string
+  platform: GamePlatform
+}
+
+export interface ISeason extends IMedia {
+  creator: string
+  length: number
+}
