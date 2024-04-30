8f752b8968be16f87050e07670d29bb9bd4f0dde
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-04-30 20:05:38 +0200

    chore: removed ''s'' infilename'

--- app/controllers/reviews_controller.ts
+++ app/controllers/reviews_controller.ts
@@ -1,4 +1,4 @@
-import ReviewService from '#services/review_services'
+import ReviewService from '#services/review_service'
 import { updateReviewValidator } from '#validators/review_validator'
 import { inject } from '@adonisjs/core'
 import type { HttpContext } from '@adonisjs/core/http'
