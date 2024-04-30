4d67dec69cb955354aa59646a2cee719196fb230
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-04-30 16:26:48 +0200

    fix: deleted getAllReviews and put validation in a try

--- app/controllers/reviews_controller.ts
+++ app/controllers/reviews_controller.ts
@@ -1,38 +1,24 @@
-import { validReviewStatus } from '#app/enums/ReviewStatus'
-import Media from '#app/models/Media'
-import Review from '#app/models/Review'
-import UpdateReviewValidator from '#app/validators/UpdateReviewValidator'
+import ReviewService from '#services/review_services'
+import { updateReviewValidator } from '#validators/review_validator'
+import { inject } from '@adonisjs/core'
 import type { HttpContext } from '@adonisjs/core/http'
 
+@inject()
 export default class ReviewsController {
-  public async getAllReviews({ response }: HttpContext) {
-    const reviews = await Review.all()
-    response.status(201)
-    return reviews
-  }
+  constructor(protected reviewService: ReviewService) {}
 
   public async updateOneReview({ request, params, response }: HttpContext) {
-    const allValidReviewStatus = validReviewStatus
     const mediaId = params.mediaId
 
-    const media = await Media.find(mediaId)
-    if (!media) {
-      return response.status(404).json("Aucun media n'a été trouvé")
-    }
-
-    const payloadValidation = await request.validate(UpdateReviewValidator)
-    const asNoValidStatus = !allValidReviewStatus.includes(payloadValidation.status)
-
-    if (asNoValidStatus) {
-      return response.status(404).json({ message: "Le statut de la review n'est pas valide" })
-    }
-
-    const reviewUpdated = payloadValidation
-
     try {
-      await media.related('review').updateOrCreate({}, reviewUpdated)
-      return response.status(200).json(reviewUpdated)
+      const payloadValidation = await request.validateUsing(updateReviewValidator)
+      const review = await this.reviewService.updateOneReview(payloadValidation, mediaId)
+
+      return response.status(201).json(review)
     } catch (error) {
+      if (error instanceof Error) {
+        return response.status(400).json({ error })
+      }
       return response.status(404).json(error)
     }
   }
