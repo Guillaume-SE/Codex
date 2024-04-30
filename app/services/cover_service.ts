dbc587472c2112d5b78688614caeaebe0aa2c1b2
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-04-30 21:04:21 +0200

    feat: add method to update cover

--- app/services/cover_service.ts
+++ app/services/cover_service.ts
@@ -4,11 +4,16 @@ import { resize, toBuffer } from '#functions/cover_modification'
 import { createAlternativeText } from '#functions/create_cover_alt_text'
 import { createFullPath } from '#functions/create_file_full_path'
 import { createFileName } from '#functions/generate_cover_name'
+import { ICover } from '#interfaces/cover_interface'
 import Cover from '#models/cover'
 import env from '#start/env'
+import { inject } from '@adonisjs/core'
 import { writeFile } from 'node:fs/promises'
+import MediaService from './media_service.js'
 
+@inject()
 export default class CoverService {
+  constructor(protected mediaService: MediaService) {}
   protected coverResizedDir: string | undefined = env.get('COVER_RESIZED_DIR')
   protected coverRawDir: string | undefined = env.get('COVER_RAW_DIR')
   protected coverExtension: string | undefined = env.get('DEFAULT_COVER_EXTENSION')
@@ -31,6 +36,23 @@ export default class CoverService {
     const saveCoverRaw = writeFile(coverRawFullPath, coverRaw)
 
     return { coverName, coverAltText }
+  }
+
+  async updateOneCover(datas: ICover, mediaId: number) {
+    const media = await this.mediaService.isMediaExist(mediaId)
+    if (!media) {
+      throw new Error('pas de media')
+    }
+
+    const cover = await this.isCoverExist(mediaId)
+    if (!cover) {
+      throw new Error('pas de cover')
+    }
+  }
+
+  async getAllCovers() {
+    const covers = await Cover.all()
+    return covers
   }
 
   async isCoverExist(mediaId: number) {
