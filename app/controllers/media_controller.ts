34a1038ce204cc49a7136cf8c77e9631f5eb8ecd
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-05-07 16:37:44 +0200

    fix: add back delete media methods

--- app/controllers/media_controller.ts
+++ app/controllers/media_controller.ts
@@ -1,8 +1,27 @@
-import Cover from '#models/cover'
 import Media from '#models/media'
+import CoverService from '#services/cover_service'
+import MediaService from '#services/media_service'
+import { inject } from '@adonisjs/core'
 import type { HttpContext } from '@adonisjs/core/http'
 
+@inject()
 export default class MediasController {
+  constructor(
+    protected mediaService: MediaService,
+    protected coverService: CoverService
+  ) {}
+
+  public async deleteOneMedia({ params, response }: HttpContext) {
+    const mediaId = params.id
+
+    try {
+      const media = await this.mediaService.deleteOneMedia(mediaId)
+      return response.status(201).json({ media })
+    } catch (error) {
+      return response.status(404).json(error)
+    }
+  }
+
   public async getAllMedia({ response }: HttpContext) {
     const medias = await Media.all()
     return response.status(201).json(medias)
@@ -11,27 +30,7 @@ export default class MediasController {
   public async getOneMediaById({ params, response }: HttpContext) {
     const mediaId = params.id
     try {
-      const media = await Media.findOrFail(mediaId)
-      response.status(201)
-      return media
-    } catch (error) {
-      return response.status(404).json(error)
-    }
-  }
-  // ADMIN
-  public async deleteOneMedia({ params, response }: HttpContext) {
-    const mediaId = params.id
-    const media = await Media.find(mediaId)
-    if (!media) {
-      return response.status(404).json('Aucun media correspondant à cet id')
-    }
-    const coverToDelete = await Cover.findBy('media_id', mediaId)
-
-    try {
-      await media.delete()
-      if (coverToDelete && coverToDelete.filename !== defaultCoverFilename) {
-        await Drive.delete(`covers/${coverToDelete.filename}`)
-      }
+      const media = await this.mediaService.getOneMediaById(mediaId)
       return response.status(201).json(media)
     } catch (error) {
       return response.status(404).json(error)
--- app/services/media_service.ts
+++ app/services/media_service.ts
@@ -1,7 +1,44 @@
 import { MediaTypes } from '#enums/MediaTypes'
+import Cover from '#models/cover'
 import Media from '#models/media'
+import env from '#start/env'
+import { inject } from '@adonisjs/core'
+import { unlink } from 'fs/promises'
+import { PathLike } from 'node:fs'
 
+@inject()
 export default class MediaService {
+  constructor() {}
+
+  protected defaultCoverFilename = env.get('DEFAULT_COVER_FILENAME')
+  protected coverResizedDir: string | PathLike = env.get('COVER_RESIZED_DIR')
+  protected coverRawDir: string | PathLike = env.get('COVER_RAW_DIR')
+
+  public async deleteOneMedia(mediaId: number) {
+    const media = await this.isMediaExist(mediaId)
+    if (!media) {
+      throw new Error('pas de media')
+    }
+
+    // direct call to model to avoid circular dependencies
+    const coverToDelete = await Cover.findBy('media_id', mediaId)
+
+    await media.delete()
+
+    if (coverToDelete) {
+      const isNotDefaultCover = coverToDelete.filename !== this.defaultCoverFilename
+      if (isNotDefaultCover) {
+        await unlink(`${this.coverResizedDir}${coverToDelete.filename}`)
+        await unlink(`${this.coverRawDir}${coverToDelete.filenameRaw}`)
+      }
+    }
+  }
+
+  async getOneMediaById(mediaId: number) {
+    const media = await Media.find(mediaId)
+    return media
+  }
+
   async isMediaAlreadyAdded(type: MediaTypes, name: string, released: string) {
     const media = await Media.query()
       .from('media')
