5fc1a304977d8d0666e6350b791f95a27e0946d0
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-05-07 16:39:10 +0200

    chore: removed brackets

--- app/controllers/covers_controller.ts
+++ app/controllers/covers_controller.ts
@@ -1,5 +1,4 @@
 import Cover from '#models/cover'
-import Media from '#models/media'
 import CoverService from '#services/cover_service'
 import env from '#start/env'
 import { updateCoverValidator } from '#validators/cover_validator'
@@ -9,12 +8,13 @@ import type { HttpContext } from '@adonisjs/core/http'
 @inject()
 export default class CoversController {
   constructor(protected coverService: CoverService) {}
+
   protected defaultCoverFilename = env.get('DEFAULT_COVER_FILENAME')
   protected defaultCoverAltText = env.get('DEFAULT_COVER_ALT_TEXT')
 
   public async getAllCovers({ response }: HttpContext) {
     const covers = await this.coverService.getAllCovers()
-    return response.status(201).json({ covers: covers })
+    return response.status(201).json({ covers })
   }
 
   public async updateOneCover({ request, params, response }: HttpContext) {
