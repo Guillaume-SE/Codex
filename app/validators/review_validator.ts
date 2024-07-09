546360bde27f28ed2573dd4d76c1d54969b676ed
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-07-09 16:56:07 +0200

    refactor: add validation method to create

--- app/validators/review_validator.ts
+++ app/validators/review_validator.ts
@@ -1,9 +1,8 @@
-import { validReviewStatus } from '#enums/ReviewStatus'
 import vine from '@vinejs/vine'
 
-export const updateReviewValidator = vine.compile(
+export const createReviewValidator = vine.compile(
   vine.object({
-    status: vine.enum(validReviewStatus),
+    statusId: vine.number().positive(),
     rating: vine.number().range([0, 10]).nullable(),
     opinion: vine.string().trim().nullable(),
     isFavorite: vine.boolean(),
