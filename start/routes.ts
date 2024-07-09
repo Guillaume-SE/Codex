e44bf6847041bdc832478de3c91bdb2771bacaa1
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-07-09 16:48:28 +0200

    refactor: add french words for some routes

--- start/routes.ts
+++ start/routes.ts
@@ -1,10 +1,11 @@
 import router from '@adonisjs/core/services/router'
 import { middleware } from './kernel.js'
+const SeriesController = () => import('#controllers/series_controller')
 const UsersController = () => import('#controllers/users_controller')
 const MediaController = () => import('#controllers/media_controller')
 const MoviesController = () => import('#controllers/movies_controller')
 const GamesController = () => import('#controllers/games_controller')
-const SeasonsController = () => import('#controllers/seasons_controller')
+const AnimeController = () => import('#controllers/anime_controller')
 const BooksController = () => import('#controllers/books_controller')
 const CoversController = () => import('#controllers/covers_controller')
 const ReviewsController = () => import('#controllers/reviews_controller')
@@ -20,54 +21,54 @@ router.get('/users/:id', [UsersController, 'show']).as('users.show')
  * medias
  */
 router.get('/media', [MediaController, 'getAllMedia']).as('media.show')
-router.get('/media/:id', [MediaController, 'getOneMediaById']).as('media.show.one')
-router.delete('/media/:id', [MediaController, 'deleteOneMedia']).as('media.delete')
+router.get('/media/:mediaId', [MediaController, 'getOneMediaById']).as('media.show.one')
+//ADMIN
+router.post('/media', [MediaController, 'addOneMedia']).as('media.add')
+router.put('/media/:mediaId', [MediaController, 'updateOneMedia']).as('media.update')
+router.delete('/media/:mediaId', [MediaController, 'deleteOneMedia']).as('media.delete')
+
 /**
  * reviews
  */
 //ADMIN
-router.put('media/:mediaId/reviews', [ReviewsController, 'updateOneReview']).as('reviews.update')
+router.post('/media/:mediaId/critique', [ReviewsController, 'addOneReview']).as('reviews.add')
+router.put('/media/:mediaId/critique', [ReviewsController, 'updateOneReview']).as('reviews.update')
 
 /**
  * movies
  */
-router.get('/movies', [MoviesController, 'getAllMovies']).as('movies.show')
-router.get('/movies/:mediaId', [MoviesController, 'getOneMovieByMediaId']).as('movies.show.one')
-//ADMIN
-router.post('/movies', [MoviesController, 'addOneMovie']).as('movies.add')
-router.put('/movies/:mediaId', [MoviesController, 'updateOneMovie']).as('movies.update')
+router.get('/films', [MoviesController, 'getAllMovies']).as('movies.show')
+router.get('/film/:mediaId', [MoviesController, 'getOneMovieByMediaId']).as('movies.show.one')
 
 /**
  * games
  */
-router.get('/games', [GamesController, 'getAllGames']).as('games.show')
-router.get('/games/:mediaId', [GamesController, 'getOneGameByMediaId']).as('games.show.one')
-//ADMIN
-router.post('/games', [GamesController, 'addOneGame']).as('games.add')
-router.put('/games/:mediaId', [GamesController, 'updateOneGame']).as('games.update')
+router.get('/jeuxvideo', [GamesController, 'getAllGames']).as('games.show')
+router.get('/jeuvideo/:mediaId', [GamesController, 'getOneGameByMediaId']).as('games.show.one')
 
 /**
- * seasons
+ * series
  */
-router.get('/seasons', [SeasonsController, 'getAllSeasons']).as('seasons.show')
-router.get('/seasons/:mediaId', [SeasonsController, 'getOneSeasonByMediaId']).as('seasons.show.one')
-//ADMIN
-router.post('/seasons', [SeasonsController, 'addOneSeason']).as('seasons.add')
-router.put('/seasons/:mediaId', [SeasonsController, 'updateOneSeason']).as('seasons.update')
+router.get('/series', [SeriesController, 'getAllSeries']).as('series.show')
+router.get('/serie/:mediaId', [SeriesController, 'getOneSeriesById']).as('series.show.one')
 
 /**
  * books
  */
-router.get('/books', [BooksController, 'getAllBooks']).as('books.show')
-router.get('/books/:mediaId', [BooksController, 'getOneBookByMediaId']).as('books.show.one')
-//ADMIN
-router.post('/books', [BooksController, 'addOneBook']).as('books.add')
-router.put('/books/:mediaId', [BooksController, 'updateOneBook']).as('books.update')
+router.get('/livres', [BooksController, 'getAllBooks']).as('books.show')
+router.get('/livre/:mediaId', [BooksController, 'getOneBookByMediaId']).as('books.show.one')
+
+/**
+ * anime
+ */
+router.get('/animes', [AnimeController, 'getAllAnime']).as('anime.show')
+router.get('/anime/:mediaId', [AnimeController, 'getOneAnimeById']).as('anime.show.one')
 
 /**
  * covers
  */
 router.get('/covers', [CoversController, 'getAllCovers'])
 //ADMIN
-router.put('/covers/:mediaId', [CoversController, 'updateOneCover'])
-router.delete('/covers/:mediaId', [CoversController, 'deleteOneCover'])
+router.post('media/:mediaId/cover', [CoversController, 'addOneCover']).as('cover.add')
+router.put('media/:mediaId/cover', [CoversController, 'updateOneCover']).as('cover.update')
+router.delete('media/:mediaId/cover', [CoversController, 'deleteOneCover']).as('cover.delete')
