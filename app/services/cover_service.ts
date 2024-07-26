8422de4fba7cdb0b0c8fcefdfde5d2325a4e91f3
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-07-26 16:15:23 +0200

    refactor: removed altText storage for alt Text

--- app/services/cover_service.ts
+++ app/services/cover_service.ts
@@ -1,15 +1,12 @@
-// import Drive from "@ioc:Adonis/Core/Drive"
-import { MediaTypes } from '#enums/MediaTypes'
 import { resize, toBuffer } from '#functions/cover_modification'
-import { createAlternativeText } from '#functions/create_cover_alt_text'
 import { createFileName } from '#functions/generate_cover_name'
 import { generateUniqueString } from '#functions/generate_unique_string'
-import { INewCover } from '#interfaces/cover_interface'
+import { ICover, INewCover } from '#interfaces/cover_interface'
 import Cover from '#models/cover'
 import Media from '#models/media'
 import env from '#start/env'
 import { inject } from '@adonisjs/core'
-import db from '@adonisjs/lucid/services/db'
+import { MultipartFile } from '@adonisjs/core/bodyparser'
 import { PathLike } from 'node:fs'
 import { rm, writeFile } from 'node:fs/promises'
 import path from 'node:path'
@@ -21,7 +18,7 @@ export default class CoverService {
   protected coverExtension: string | PathLike = env.get('DEFAULT_COVER_EXTENSION')
   protected defaultCoverFilename = env.get('DEFAULT_COVER_FILENAME')
 
-  async addCoverInfos(cover: INewCover, mediaId: number) {
+  async addCoverInfos(datas: INewCover, mediaId: number) {
     const media = await Media.find(mediaId)
     if (!media) {
       throw new Error("Le media n'existe pas")
@@ -33,39 +30,25 @@ export default class CoverService {
       throw new Error("La media a déjà des infos pour sa cover, redirection vers l'update")
     }
 
-    // let coverResizedFilename = this.defaultCoverFilename
-    // let coverRawFilename = this.defaultCoverFilename
-    // let coverTmpPath = null
-    // if (cover.cover) {
     const coverName = generateUniqueString()
     const coverResizedFilename = createFileName(coverName, this.coverExtension, false)
     const coverRawFilename = createFileName(coverName, this.coverExtension, true)
-    const coverTmpPath = cover.cover.tmpPath
-    // }
+    const coverTmpPath = datas.cover.tmpPath
 
-    const coverInfos = {
+    const coverInfos: ICover = {
       resizedVersion: coverResizedFilename,
       rawVersion: coverRawFilename,
     }
 
-    // const cover = new Cover()
-    // await db.transaction(async (trx) => {
-    //   cover.useTransaction(trx)
-    //   await cover
-    //     .merge({
-    //       mediaId: media.id,
-    //       resizedVersion: coverResizedFilename,
-    //       rawVersion: coverRawFilename,
-    //     })
-    //     .save()
-    // })
-
     await media.related('cover').create(coverInfos)
 
-    return { cover, coverPath: coverTmpPath }
+    return { coverInfos, coverTmpPath }
   }
 
-  async saveCover(resizedFilename: string, rawFilename: string, tmpPath: string) {
+  async saveCover(resizedFilename: string, rawFilename: string, tmpPath: string | undefined) {
+    if (tmpPath === undefined) {
+      throw new Error("Aucun chemin disponible pour l'image")
+    }
     const coverResized = await resize(tmpPath, 340)
     const coverResizedFullPath = `${this.coverResizedDir}${resizedFilename}`
     await writeFile(coverResizedFullPath, coverResized)
