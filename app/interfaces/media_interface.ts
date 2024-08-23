03b28af1f4ef0bcd54a6194a966153b55ee38c32
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-08-23 16:46:30 +0200

    refactor: add interface to represente a complete media card

--- app/interfaces/media_interface.ts
+++ app/interfaces/media_interface.ts
@@ -1,23 +1,7 @@
-export interface INewMediaPayload {
+export interface IMediaPayload {
   mediaParentId: number | null
   statusId: number
-  categoryId: number
-  typeId: number
-  name: string
-  alternativeName: string | null
-  released: string | null
-  synopsis: string | null
-  genresIds: Array<number>
-  platformId?: number | null
-  duration?: number | null
-  seriesSeasonLength?: number | null
-  animeSeasonLength?: number | null
-  pages?: number | null
-}
-
-export interface IUpdatedMediaPayload {
-  mediaParentId: number | null
-  statusId: number
+  categoryId?: number
   typeId: number
   name: string
   alternativeName: string | null
@@ -40,4 +24,43 @@ export interface IMedia {
   alternativeName: string | null
   released: string | null
   synopsis: string | null
+}
+
+export interface ICompleteMedia {
+  id: number
+  mediaParentId: number | null
+  status: string
+  category: string
+  type: string
+  name: string
+  alternativeName: string | null
+  released: string | null
+  synopsis: string | null
+  genres?: Array<string>
+  contributors?: Record<string, string[]>
+  gameInfos?: {
+    platform: string | null
+  }
+  bookInfos?: {
+    pages: number | null
+  }
+  movieInfos?: {
+    duration: number | null
+  }
+  animeInfos?: {
+    seasonLength: number | null
+  }
+  seriesInfos?: {
+    seasonLength: number | null
+  }
+  review?: {
+    rating: number | null
+    opinion: string | null
+    isFavorite: boolean | null
+    lastUpdate: number | null
+  }
+  cover?: {
+    resized: string | null
+    original: string | null
+  }
 }
