a80af5d2120b1512a9de55d52e2f85ded0e245f8
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-08-24 12:05:22 +0200

    refactor: add custom type to gameList

--- app/controllers/games_controller.ts
+++ app/controllers/games_controller.ts
@@ -1,3 +1,4 @@
+import { ICompleteMediaCard } from '#interfaces/media_interface'
 import GameService from '#services/game_service'
 import MediaService from '#services/media_service'
 import { inject } from '@adonisjs/core'
@@ -13,7 +14,7 @@ export default class GamesController {
   public async getAllGames({ response }: HttpContext) {
     try {
       const mediaList = await this.mediaService.getAllMedia()
-      const gamesList = await this.gameService.getAllGames(mediaList)
+      const gamesList: ICompleteMediaCard[] = await this.gameService.getAllGames(mediaList)
       return response.status(200).json(gamesList)
     } catch (error) {
       return response.status(404).json(error)
