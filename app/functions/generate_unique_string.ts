51f8995ee844b5a9669eddb76b6f921cc4a0d9ca
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-08-27 17:03:49 +0200

    refactor: no longer used or moved

--- app/functions/generate_unique_string.ts
+++
@@ -1,8 +0,0 @@
-import string from '@adonisjs/core/helpers/string'
-
-export function generateUniqueString() {
-  const timestamp = new Date().getTime().toString()
-  const randomString = string.random(15)
-
-  return `${timestamp}-${randomString}`
-}
