479ce21babaaff143e2bac8fcf32b3da0a6356f9
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-08-28 15:53:19 +0200

    refactor: sync add and update to a manage method

--- app/services/review_service.ts
+++ app/services/review_service.ts
@@ -5,33 +5,14 @@ import { inject } from '@adonisjs/core'
 
 @inject()
 export default class ReviewsController {
-  async addOneReview(review: IReview, mediaId: number) {
+  async manageOneReview(review: IReview, mediaId: number) {
     const media = await Media.find(mediaId)
     if (!media) {
       throw new Error("Le media n'existe pas")
     }
-    const isAlreadyReviewed = await Review.findBy('mediaId', mediaId)
-    if (isAlreadyReviewed) {
-      throw new Error('Ce media possède déjà une critique')
-    }
 
-    await media.related('review').create(review)
-
-    return review
-  }
-
-  async updateOneReview(datas: IReview, mediaId: number) {
-    const media = await Media.findOrFail(mediaId)
-    if (!media) {
-      throw new Error('pas de media')
-    }
-
-    const review = await this.getOneReviewByMediaId(mediaId)
-    if (!review) {
-      throw new Error('pas de review')
-    }
-
-    review.merge(datas).save()
+    const searchPayload = { mediaId: media.id }
+    await media.related('review').updateOrCreate(searchPayload, review)
 
     return review
   }
