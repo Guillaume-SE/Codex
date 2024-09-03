df17e34e87ad63869431f6c66d7051078579c0f8
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-09-03 15:38:56 +0200

    refactor: removed read/update and delete methods

--- app/controllers/media_categories_controller.ts
+++ app/controllers/media_categories_controller.ts
@@ -1,42 +1,8 @@
 import MediaCategoryService from '#services/media_category_service'
-import { manageMediaCategoryValidator } from '#validators/media_category_validator'
 import { inject } from '@adonisjs/core'
-import type { HttpContext } from '@adonisjs/core/http'
+// import type { HttpContext } from '@adonisjs/core/http'
 
 @inject()
 export default class MediaCategoriesController {
   constructor(readonly mediaCategoryService: MediaCategoryService) {}
-
-  public async addOneCategory({ request, response }: HttpContext) {
-    try {
-      const data = await request.validateUsing(manageMediaCategoryValidator)
-      const category = await this.mediaCategoryService.addOne(data)
-      return response.status(201).json(category)
-    } catch (error) {
-      return response.status(400).json({ error, customError: error.message })
-    }
-  }
-
-  public async updateOneCategory({ params, request, response }: HttpContext) {
-    const categoryId = params.categoryId
-
-    try {
-      const data = await request.validateUsing(manageMediaCategoryValidator)
-      const category = await this.mediaCategoryService.updateOne(data, categoryId)
-      return response.status(201).json(category)
-    } catch (error) {
-      return response.status(400).json({ error, customError: error.message })
-    }
-  }
-
-  public async deleteOneCategory({ params, response }: HttpContext) {
-    const categoryId = params.categoryId
-
-    try {
-      await this.mediaCategoryService.deleteOne(categoryId)
-      return response.status(202)
-    } catch (error) {
-      return response.status(400).json({ error, customError: error.message })
-    }
-  }
 }
--- app/services/media_category_service.ts
+++ app/services/media_category_service.ts
@@ -1,48 +1,4 @@
-import { IMediaCategory } from '#interfaces/media_category_interface'
-import Media from '#models/media'
-import MediaCategory from '#models/media_category'
 import { inject } from '@adonisjs/core'
 
 @inject()
-export default class MediaCategoryService {
-  public async addOne(category: IMediaCategory) {
-    const existingSameCategory = await MediaCategory.findBy('name', category.name)
-    if (existingSameCategory) {
-      throw new Error('Cette catégorie existe déjà')
-    }
-
-    const newCategory = await MediaCategory.create(category)
-
-    return newCategory
-  }
-
-  public async updateOne(updatedCategory: IMediaCategory, categoryId: number) {
-    const validSelectedCategory = await MediaCategory.find(categoryId)
-    if (!validSelectedCategory) {
-      throw new Error("La catégorie n'existe pas")
-    }
-
-    const isUpdatedCategoryAlreadyExist = await MediaCategory.findBy('name', updatedCategory.name)
-    if (isUpdatedCategoryAlreadyExist) {
-      throw new Error('Cette catégorie existe déjà')
-    }
-
-    await validSelectedCategory.merge(updatedCategory).save()
-
-    return updatedCategory
-  }
-
-  public async deleteOne(categoryId: number) {
-    const validSelectedCategory = await MediaCategory.find(categoryId)
-    if (!validSelectedCategory) {
-      throw new Error("La catégorie selectionnée n'existe pas")
-    }
-
-    const mediaUsingSelectedCategory = await Media.findBy('categoryId', validSelectedCategory.id)
-    if (mediaUsingSelectedCategory) {
-      throw new Error('Impossible de supprimer car un ou plusieurs media utilisent cette catégorie')
-    }
-
-    await validSelectedCategory.delete()
-  }
-}
+export default class MediaCategoryService {}
