2b04b9402230d4b3919e2c7f054684c6ac2e7ca6
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-09-18 21:58:24 +0200

    refactor: added valid extensions in raw instead of enum

--- app/validators/cover_validator.ts
+++ app/validators/cover_validator.ts
@@ -1,11 +1,10 @@
-import { validCoverFileExtension } from '#enums/FileExtension'
 import vine from '@vinejs/vine'
 
 export const manageCoverValidator = vine.compile(
   vine.object({
     cover: vine.file({
       size: '2mb',
-      extnames: validCoverFileExtension,
+      extnames: ['png', 'webp', 'jpg', 'jpeg'],
     }),
   })
 )
