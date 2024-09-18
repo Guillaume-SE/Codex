21be1334f79de5f139259acbd620aaddeeb8f3da
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-09-18 22:01:46 +0200

    refactor: make createfilename naming more simple

--- app/classes/CoverUtils.ts
+++ app/classes/CoverUtils.ts
@@ -1,15 +1,23 @@
-import string from '@adonisjs/core/helpers/string'
+import env from '#start/env'
+import { MultipartFile } from '@adonisjs/core/bodyparser'
+import { cuid } from '@adonisjs/core/helpers'
 import { PathLike } from 'fs'
 import sharp from 'sharp'
 
+interface ProcessImageOptions {
+  width: number
+  height: number
+  format: keyof sharp.FormatEnum
+}
+
 export class CoverUtils {
+  static DEFAULT_COVER_EXTENSION: string | PathLike = env.get('DEFAULT_COVER_EXTENSION')
+
   static resize(path: string, width: number) {
     const coverResized = sharp(path)
       .resize({
-        // height: 500,
+        height: 450,
         width: width,
-        // fit: 'cover',
-        // position: 'top',
       })
       .toFormat('jpg', { mozjpeg: true })
       .toBuffer()
@@ -20,19 +28,33 @@ export class CoverUtils {
     return sharp(path).toBuffer()
   }
 
-  static generateUniqueString() {
-    const timestamp = new Date().getTime().toString()
-    const randomString = string.random(20)
+  // static async processImage(file: MultipartFile, options?: ProcessImageOptions) {
+  //   if (file.tmpPath) {
+  //     const image = sharp(file.tmpPath)
 
-    return `${timestamp}-${randomString}`
-  }
+  //     if (options?.width || options?.height) {
+  //       image.resize({
+  //         width: options.width,
+  //         height: options.height,
+  //         fit: 'cover',
+  //       })
+  //     }
 
-  static createFilenames(fileExtension: string | PathLike) {
-    const uniqueString = this.generateUniqueString()
+  //     if (options?.format) {
+  //       image.toFormat(options.format)
+  //     }
+
+  //     const buffer = await image.toBuffer()
+  //     await sharp(buffer).toFile(file.tmpPath)
+  //   }
+  // }
+
+  static createFilenames() {
+    const key = cuid()
 
     return {
-      original: `${uniqueString}${fileExtension}`,
-      resized: `${uniqueString}-resized${fileExtension}`,
+      original: `${key}${this.DEFAULT_COVER_EXTENSION}`,
+      resized: `${key}-resized${this.DEFAULT_COVER_EXTENSION}`,
     }
   }
 }
