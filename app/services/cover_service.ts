aff2cc777758497d7aec1b1a7212d25ebbc87dc8
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-09-18 21:59:34 +0200

    refactor: changed variable name for env imports

--- app/services/cover_service.ts
+++ app/services/cover_service.ts
@@ -1,20 +1,20 @@
 import { CoverUtils } from '#classes/CoverUtils'
-import { ICover, INewCover } from '#interfaces/cover_interface'
+import type { ICover } from '#interfaces/cover_interface'
 import Cover from '#models/cover'
 import Media from '#models/media'
 import env from '#start/env'
 import { inject } from '@adonisjs/core'
+import { MultipartFile } from '@adonisjs/core/bodyparser'
 import { PathLike } from 'node:fs'
 import { rm, writeFile } from 'node:fs/promises'
 import path from 'node:path'
 
 @inject()
 export default class CoverService {
-  protected coverResizedDir: string | PathLike = env.get('COVER_RESIZED_DIR')
-  protected coverOriginalDir: string | PathLike = env.get('COVER_ORIGINAL_DIR')
-  protected coverExtension: string | PathLike = env.get('DEFAULT_COVER_EXTENSION')
+  protected RESIZED_COVER_DIR: string | PathLike = env.get('RESIZED_COVER_DIR')
+  protected ORIGINAL_COVER_DIR: string | PathLike = env.get('ORIGINAL_COVER_DIR')
 
-  async saveCoverData(data: INewCover, mediaId: number) {
+  async saveStoredCoverFilenames(file: MultipartFile, mediaId: number) {
     const media = await Media.find(mediaId)
     if (!media) {
       throw new Error("Le media n'existe pas")
@@ -22,8 +22,8 @@ export default class CoverService {
 
     const existingCover = await Cover.findBy('media_id', mediaId)
 
-    const coverFilenames = CoverUtils.createFilenames(this.coverExtension)
-    const coverTmpPath = data.cover.tmpPath
+    const coverFilenames = CoverUtils.createFilenames()
+    const coverTmpPath = file.tmpPath
 
     const coverInfos: ICover = {
       resizedCoverFilename: coverFilenames.resized,
@@ -43,21 +43,17 @@ export default class CoverService {
     return { coverInfos, coverTmpPath }
   }
 
-  async saveCoverFile(
-    resizedFilename: string,
-    originalFilename: string,
-    tmpPath: string | undefined
-  ) {
+  async storeCover(resizedFilename: string, originalFilename: string, tmpPath: string | undefined) {
     if (tmpPath === undefined) {
       throw new Error("Aucun chemin disponible pour l'image")
     }
     const coverResized = await CoverUtils.resize(tmpPath, 340)
-    const coverResizedFullPath = `${this.coverResizedDir}${resizedFilename}`
+    const coverResizedFullPath = `${this.RESIZED_COVER_DIR}${resizedFilename}`
     await writeFile(coverResizedFullPath, coverResized)
 
     //to keep a clean original version
     const coverOriginal = await CoverUtils.toBuffer(tmpPath)
-    const coverOriginalFullPath = `${this.coverOriginalDir}${originalFilename}`
+    const coverOriginalFullPath = `${this.ORIGINAL_COVER_DIR}${originalFilename}`
     await writeFile(coverOriginalFullPath, coverOriginal)
 
     return { coverResizedFullPath, coverOriginalFullPath }
@@ -75,8 +71,8 @@ export default class CoverService {
   }
 
   async deleteCoverByFilenames(resizedCoverfilename: string, originalCoverFilename: string) {
-    const coverResizedFullpath = path.join(`${this.coverResizedDir}${resizedCoverfilename}`)
-    const coverOriginalFullpath = path.join(`${this.coverOriginalDir}${originalCoverFilename}`)
+    const coverResizedFullpath = path.join(`${this.RESIZED_COVER_DIR}${resizedCoverfilename}`)
+    const coverOriginalFullpath = path.join(`${this.ORIGINAL_COVER_DIR}${originalCoverFilename}`)
     await rm(coverResizedFullpath, { force: true })
     await rm(coverOriginalFullpath, { force: true })
   }
