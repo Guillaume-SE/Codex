b0ac985b14c02392deefe600338529414ca6e399
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-09-18 22:00:31 +0200

    refactor: removed unnecessary interface

--- app/interfaces/cover_interface.ts
+++ app/interfaces/cover_interface.ts
@@ -1,9 +1,3 @@
-import { MultipartFile } from '@adonisjs/core/bodyparser'
-
-export interface INewCover {
-  cover: MultipartFile
-}
-
 export interface ICover {
   resizedCoverFilename: string
   originalCoverFilename: string
