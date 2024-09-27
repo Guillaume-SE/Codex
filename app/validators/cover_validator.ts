0add474e5dc0d91ecad2afd5db2eaae91042e8c1
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-09-27 21:23:55 +0200

    refactor: added validation for params

--- app/validators/cover_validator.ts
+++ app/validators/cover_validator.ts
@@ -7,10 +7,7 @@ export const manageCoverValidator = vine.compile(
       extnames: ['png', 'webp', 'jpg', 'jpeg'],
     }),
     params: vine.object({
-      mediaId: vine.number().exists(async (db, value) => {
-        const mediaExist = await db.from('media').where('id', value).first()
-        return !!mediaExist
-      }),
+      mediaId: vine.number().isExists({ table: 'media', column: 'id' }),
     }),
   })
 )
