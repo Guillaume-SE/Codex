cfec5f08923fdc41acac68345be985a04d890223
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-10-01 20:51:32 +0200

    refactor: added rules and removed delete type validation

--- app/validators/media_type_validator.ts
+++ app/validators/media_type_validator.ts
@@ -1,14 +1,40 @@
 import vine from '@vinejs/vine'
 
-export const createMediaTypeValidator = vine.compile(
+export const createMediaTypeValidator = vine.withMetaData<{ categoryId: number }>().compile(
   vine.object({
-    name: vine.string().trim(),
-    categoryId: vine.number().positive(),
+    name: vine
+      .string()
+      .trim()
+      .unique(async (db, value, field) => {
+        const typeIsUnique = await db
+          .from('media_types')
+          .where('name', value)
+          .andWhere('category_id', '=', field.meta.categoryId)
+          .first()
+        return !typeIsUnique
+      }),
+    categoryId: vine.number().isExists({ table: 'media_categories', column: 'id' }),
   })
 )
 
-export const updateMediaTypeValidator = vine.compile(
+export const updateMediaTypeValidator = vine.withMetaData<{ categoryId: number }>().compile(
   vine.object({
-    name: vine.string().trim(),
+    // params
+    params: vine.object({
+      typeId: vine.number().isExists({ table: 'media_types', column: 'id' }),
+    }),
+    // body
+    name: vine
+      .string()
+      .trim()
+      .unique(async (db, value, field) => {
+        const typeIsUnique = await db
+          .from('media_types')
+          .where('name', value)
+          .andWhere('category_id', '=', field.meta.categoryId)
+          .first()
+        return !typeIsUnique
+      }),
+    categoryId: vine.number().isExists({ table: 'media_categories', column: 'id' }),
   })
 )
