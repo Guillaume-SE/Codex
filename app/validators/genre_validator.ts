e0831fa51b82a31c38ea3b974003c87e4604df41
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-10-01 16:21:58 +0200

    refactor: moved validation rules from the service

--- app/validators/genre_validator.ts
+++ app/validators/genre_validator.ts
@@ -1,14 +1,47 @@
 import vine from '@vinejs/vine'
 
-export const createGenreValidator = vine.compile(
+export const createGenreValidator = vine.withMetaData<{ categoryId: number }>().compile(
   vine.object({
-    name: vine.string().trim(),
-    categoryId: vine.number().positive(),
+    name: vine
+      .string()
+      .trim()
+      .unique(async (db, value, field) => {
+        const genreIsUnique = await db
+          .from('genres')
+          .where('name', value)
+          .andWhere('category_id', '=', field.meta.categoryId)
+          .first()
+        return !genreIsUnique
+      }),
+    categoryId: vine.number().isExists({ table: 'media_categories', column: 'id' }),
   })
 )
 
 export const updateGenreValidator = vine.compile(
   vine.object({
-    name: vine.string().trim(),
+    name: vine
+      .string()
+      .trim()
+      .unique(async (db, value, field) => {
+        const genreIsUnique = await db
+          .from('genres')
+          .where('name', value)
+          .andWhere('category_id', '=', field.meta.categoryId)
+          .first()
+        return !genreIsUnique
+      }),
+    categoryId: vine.number().isExists({ table: 'media_categories', column: 'id' }),
+    // params
+    params: vine.object({
+      genreId: vine.number().isExists({ table: 'genres', column: 'id' }),
+    }),
+  })
+)
+
+export const deleteGenreValidator = vine.compile(
+  vine.object({
+    params: vine.object({
+      genreId: vine.number().isExists({ table: 'genres', column: 'id' }),
+    }),
   })
 )
