35ddc5cccddafe4f28457cc711cfc04e7e5fbd90
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-10-01 16:22:20 +0200

    refactor: moved validation rules to the validator

--- app/services/genre_service.ts
+++ app/services/genre_service.ts
@@ -1,56 +1,26 @@
 import type { IGenre } from '#interfaces/genre_interface'
 import Genre from '#models/genre'
-import MediaCategory from '#models/media_category'
 import { inject } from '@adonisjs/core'
 
 @inject()
 export default class GenreService {
-  public async addOne(genre: IGenre) {
-    const validSelectedCategory: MediaCategory | null = await MediaCategory.find(genre.categoryId)
-    if (!validSelectedCategory) {
-      throw new Error('Aucune catégorie ne correspond')
-    }
-
-    const matchingGenres = await Genre.query()
-      .select('*')
-      .from('genres')
-      .whereIn(['name', 'categoryId'], [[genre.name, validSelectedCategory.id]])
-    const isSelectedGenreAlreadyAdded = matchingGenres.length > 0
-    if (isSelectedGenreAlreadyAdded) {
-      throw new Error(`Ce genre a déjà été ajouté pour la catégorie ${validSelectedCategory.name}`)
-    }
-
+  public async store(genre: IGenre) {
     const newGenre = await Genre.create(genre)
 
     return newGenre
   }
 
-  public async updateOne(updatedGenre: IGenre, genreId: number) {
-    const validSelectedGenre: Genre | null = await Genre.find(genreId)
-    if (!validSelectedGenre) {
-      throw new Error('Aucun genre trouvé')
-    }
+  public async update(updatedGenre: IGenre, genreId: number) {
+    const genre = await Genre.findOrFail(genreId)
 
-    const matchingGenres = await Genre.query()
-      .select('*')
-      .from('genres')
-      .whereIn(['name', 'categoryId'], [[updatedGenre.name, validSelectedGenre.categoryId]])
-    const isSelectedGenreAlreadyAdded = matchingGenres.length > 0
-    if (isSelectedGenreAlreadyAdded) {
-      throw new Error('Ce genre a déjà été ajouté pour cette catégorie')
-    }
+    await genre.merge(updatedGenre).save()
 
-    await validSelectedGenre.merge(updatedGenre).save()
-
-    return updatedGenre
+    return genre
   }
 
-  public async deleteOne(genreId: number) {
-    const validSelectedGenre: Genre | null = await Genre.find(genreId)
-    if (!validSelectedGenre) {
-      throw new Error("Le genre selectionné n'existe pas")
-    }
+  public async delete(genreId: number): Promise<void> {
+    const genre = await Genre.findOrFail(genreId)
 
-    await validSelectedGenre.delete()
+    await genre.delete()
   }
 }
