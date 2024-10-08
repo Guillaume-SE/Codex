38b7752b6e86b8eb5f99b78e8fbf0921f9fc0621
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-10-08 21:33:55 +0200

    refactor: added contributor validation

--- app/validators/contributor_role_validator.ts
+++ app/validators/contributor_role_validator.ts
@@ -1,7 +1,39 @@
 import vine from '@vinejs/vine'
 
-export const manageContributorRoleValidator = vine.compile(
+export const createContributorRoleValidator = vine.compile(
   vine.object({
-    name: vine.string().trim(),
+    name: vine
+      .string()
+      .trim()
+      .unique(async (db, value) => {
+        const roleIsUnique = await db.from('contributors_roles').where('name', value).first()
+        return !roleIsUnique
+      }),
+  })
+)
+
+export const updateContributorRoleValidator = vine.compile(
+  vine.object({
+    // params
+    params: vine.object({
+      roleId: vine.number().isExists({ table: 'contributors_roles', column: 'id' }),
+    }),
+    // body
+    name: vine
+      .string()
+      .trim()
+      .unique(async (db, value) => {
+        const roleIsUnique = await db.from('contributors_roles').where('name', value).first()
+        return !roleIsUnique
+      }),
+  })
+)
+
+export const deleteContributorRoleValidator = vine.compile(
+  vine.object({
+    // params
+    params: vine.object({
+      roleId: vine.number().isExists({ table: 'contributors_roles', column: 'id' }),
+    }),
   })
 )
