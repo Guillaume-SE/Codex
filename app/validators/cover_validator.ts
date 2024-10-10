d128b9f2bd6c09b01c56b3c6412df6d294ff7cb2
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-10-10 15:15:14 +0200

    refactor: added "avif" extension

--- app/validators/cover_validator.ts
+++ app/validators/cover_validator.ts
@@ -4,7 +4,7 @@ export const manageCoverValidator = vine.compile(
   vine.object({
     cover: vine.file({
       size: '2mb',
-      extnames: ['png', 'webp', 'jpg', 'jpeg'],
+      extnames: ['png', 'webp', 'jpg', 'jpeg', 'avif'],
     }),
     params: vine.object({
       mediaId: vine.number().isExists({ table: 'media', column: 'id' }),
