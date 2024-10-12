cba3b64439173b50a316c5f961dddd0c4d7abe0d
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-10-12 20:41:26 +0200

    feat: added a generic layout for all pages

---
+++ inertia/layouts/AppLayout.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { Link } from '@inertiajs/vue3'
+</script>
+
+<template>
+  <main>
+    <header>
+      <nav class="navbar">
+        <div>
+          <Link href="/">Accueil</Link>
+        </div>
+        <div>
+          <Link href="/games">Jeux vidéo</Link>
+        </div>
+        <div>
+          <Link href="/movies">Films</Link>
+        </div>
+        <div>
+          <Link href="/series">Séries</Link>
+        </div>
+        <div>
+          <Link href="/anime">Anime</Link>
+        </div>
+        <div>
+          <Link href="/books">Livres</Link>
+        </div>
+        <div>Connexion</div>
+      </nav>
+    </header>
+    <div>
+      <slot />
+    </div>
+  </main>
+</template>
