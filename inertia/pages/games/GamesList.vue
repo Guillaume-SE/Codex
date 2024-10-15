6e5a6c4fee7a84024d022ba018a280804627918d
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-10-15 10:12:57 +0200

    refactor: now using MediaCard component to render card

---
+++ inertia/pages/games/GamesList.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { IGameMediaFormatted } from '#interfaces/media_formatted_interface'
+import MediaCard from '../components/MediaCard.vue'
+
+defineProps<{
+  gamesList: IGameMediaFormatted[]
+}>()
+</script>
+
+<template>
+  <div class="cards-container">
+    <MediaCard
+      v-for="game in gamesList"
+      :key="game.id"
+      :media="game"
+      :mediaType="'jeu vidéo'"
+      :mediaInfos="{ platform: game.gameInfos?.platform }"
+    />
+  </div>
+</template>
