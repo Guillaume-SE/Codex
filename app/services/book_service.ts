107c256dc068df3ecc13dce878023c20596b22f5
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-10-16 20:53:06 +0200

    refactor: now use BaseMediaFormatted to type media list

--- app/services/book_service.ts
+++ app/services/book_service.ts
@@ -1,9 +1,9 @@
-import type { IMedia } from '#interfaces/media_interface'
+import type { IBaseMediaFormatted } from '#interfaces/media_formatted_interface'
 import { inject } from '@adonisjs/core'
 
 @inject()
 export default class BookService {
-  public async getAllBooks(mediaList: IMedia[]) {
+  public async getAllBooks(mediaList: IBaseMediaFormatted[]) {
     const booksList = mediaList.filter((media) => media.category === 'Livre')
 
     return booksList
