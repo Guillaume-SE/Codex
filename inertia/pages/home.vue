ddb686bd27c9e3d10dc47040040b75d46256f6a4
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-10-22 19:37:10 +0200

    refactor: can now customise meta title

--- inertia/pages/home.vue
+++ inertia/pages/home.vue
@@ -1,3 +1,10 @@
+<script setup>
+import AppHead from '~/components/AppHead.vue'
+</script>
+
 <template>
-  <p>Home</p>
+  <AppHead title="Accueil" />
+  <div>
+    <p>Home</p>
+  </div>
 </template>
