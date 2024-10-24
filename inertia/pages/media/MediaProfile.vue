fa3fd928ed866f1082944243234377d8f16f076d
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-10-24 16:05:04 +0200

    refactor: moved file to pages folder

---
+++ inertia/pages/media/MediaProfile.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import type {
+  IAnimeMediaFormatted,
+  IBookMediaFormatted,
+  IGameMediaFormatted,
+  IMovieMediaFormatted,
+  ISeriesMediaFormatted,
+} from '#interfaces/media_formatted_interface'
+import AppHead from '~/components/AppHead.vue'
+
+const props = defineProps<{
+  media:
+    | IGameMediaFormatted
+    | IMovieMediaFormatted
+    | ISeriesMediaFormatted
+    | IAnimeMediaFormatted
+    | IBookMediaFormatted
+}>()
+</script>
+
+<template>
+  <AppHead :title="media.name" />
+  <div class="media-profile-container">
+    <div v-if="media.cover">
+      <img
+        class="img-large"
+        loading="lazy"
+        :src="`/storage/${media.cover.mediumUrl}`"
+        :srcset="`/storage/${media.cover.mediumUrl}, /storage/${media.cover.largeUrl} 2x`"
+        :alt="`affiche du ${media.name}`"
+      />
+    </div>
+    <h2>{{ media.name }}</h2>
+  </div>
+</template>
