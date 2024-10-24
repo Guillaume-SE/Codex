6b41be15991d0216878c729a0bf6ffbe26eab769
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-10-24 16:09:02 +0200

    refactor: commented status in eng to keep a reference

--- app/enums/MediaStatuses.ts
+++ app/enums/MediaStatuses.ts
@@ -1,4 +1,5 @@
 const status = ['En cours', 'Terminé', 'En pause', 'Abandonné', 'Prévu', 'Attendu'] as const
+// const status = ['in progress', 'completed', 'paused', 'dropped', 'planned', 'anticipated'] as const
 
 export type MediaStatus = (typeof status)[number]
 
