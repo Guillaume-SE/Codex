bdfe171ec61e75c69901e014feabfe802b50a301
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-10-24 16:04:43 +0200

    refactor: moved MediaList to pages folder

---
+++ inertia/pages/media/MediaList.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import type { MediaCategories } from '#enums/MediaCategories'
+import {
+  IAnimeMediaFormatted,
+  IBookMediaFormatted,
+  IGameMediaFormatted,
+  IMovieMediaFormatted,
+  ISeriesMediaFormatted,
+} from '#interfaces/media_formatted_interface'
+import AppHead from '~/components/AppHead.vue'
+import MediaCard from '~/components/MediaCard.vue'
+
+defineProps<{
+  title: string
+  mediaList:
+    | IGameMediaFormatted[]
+    | IMovieMediaFormatted[]
+    | ISeriesMediaFormatted[]
+    | IAnimeMediaFormatted[]
+    | IBookMediaFormatted[]
+  mediaType: string
+  mediaCategory: MediaCategories
+}>()
+</script>
+
+<template>
+  <AppHead :title="title" />
+  <div v-if="mediaList.length > 0" class="media-card-container">
+    <MediaCard
+      v-for="media in mediaList"
+      :key="media.id"
+      :media="media"
+      :mediaType="mediaType"
+      :mediaCategory="mediaCategory"
+    />
+  </div>
+  <div v-else>
+    <p>Aucun résultat à afficher</p>
+  </div>
+</template>
