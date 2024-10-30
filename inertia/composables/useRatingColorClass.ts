b751df79cb709638e9b35e000b3b0bb147d32df7
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-10-30 20:57:01 +0100

    feat: created composable to change rating box dynamicaly

---
+++ inertia/composables/useRatingColorClass.ts
@@ -0,0 +1,17 @@
+export function useRatingColorClass(rating: number | null | undefined): string {
+  if (rating === null || rating === undefined) {
+    return 'rating-default'
+  }
+
+  if (rating >= 0 && rating <= 4) {
+    return 'rating-bad'
+  }
+  if (rating >= 5 && rating <= 7) {
+    return 'rating-mid'
+  }
+  if (rating >= 8 && rating <= 10) {
+    return 'rating-good'
+  }
+
+  return 'rating-default'
+}
