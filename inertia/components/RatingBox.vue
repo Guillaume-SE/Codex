4f0060fae2935000a46b192d97d27e79541f938b
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-10-30 21:00:25 +0100

    chore: can now use rating box as a componenent

---
+++ inertia/components/RatingBox.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useRatingColorClass } from '~/composables/useRatingColorClass'
+
+const props = defineProps<{
+  rating: number | null | undefined
+}>()
+
+const ratingColor = computed(() => useRatingColorClass(props.rating))
+</script>
+
+<template>
+  <div :class="['rating-number-container', ratingColor]">
+    <span class="rating-number">{{ rating ? rating : '/' }}</span>
+  </div>
+</template>
