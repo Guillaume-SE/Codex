281a3bfaf675615b226fbb8f0b50e020abc73190
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-11-05 15:47:13 +0100

    refactor: globally reduced size

--- inertia/components/icons/StatusProgressIcon.vue
+++ inertia/components/icons/StatusProgressIcon.vue
@@ -2,8 +2,8 @@
   <svg
     class="status-progress-icon"
     xmlns="http://www.w3.org/2000/svg"
-    width="35"
-    height="35"
+    width="30"
+    height="30"
     viewBox="0 0 15 15"
   >
     <path fill="currentColor" d="M9.875 7.5a2.375 2.375 0 1 1-4.75 0a2.375 2.375 0 0 1 4.75 0" />
