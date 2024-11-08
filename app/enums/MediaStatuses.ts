f49a960cd781eb93c4031d46f956cd7969cf04ce
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-11-08 21:48:24 +0100

    refactor: lowercased terms

--- app/enums/MediaStatuses.ts
+++ app/enums/MediaStatuses.ts
@@ -1,6 +1,6 @@
-const status = ['En cours', 'Terminé', 'En pause', 'Abandonné', 'Prévu', 'Attendu'] as const
+const status = ['en cours', 'terminé', 'en pause', 'abandonné', 'prévu', 'attendu'] as const
 // const status = ['in progress', 'completed', 'paused', 'dropped', 'planned', 'anticipated'] as const
 
-export type MediaStatus = (typeof status)[number]
+export type MediaStatuses = (typeof status)[number]
 
 export { status as validMediaStatus }
