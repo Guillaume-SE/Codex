082b08c78f62fd011d7ab7322b333859445d8632
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-11-08 21:48:41 +0100

    chore: added french variation for categories

--- app/enums/MediaCategories.ts
+++ app/enums/MediaCategories.ts
@@ -1,2 +1,4 @@
 const categories = ['game', 'movie', 'series', 'anime', 'book']
+const categoriesFr = ['jeu', 'film', 'série', 'anime', 'livre']
 export type MediaCategories = (typeof categories)[number]
+export type MediaCategoriesFr = (typeof categoriesFr)[number]
