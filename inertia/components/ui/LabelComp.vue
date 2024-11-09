5a66698959822a3302aff7764c0d59792c02cccb
Guillaume-SE <guillaume.seure-even@3wa.io>
2024-11-09 15:01:51 +0100

    chore: cannow use reusable label input

---
+++ inertia/components/ui/LabelComp.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps<{
+  textPosition: 'up' | 'down'
+  text: string
+}>()
+</script>
+
+<template>
+  <template v-if="textPosition === 'up'">
+    <label>
+      {{ text }}
+      <slot />
+    </label>
+  </template>
+
+  <template v-else>
+    <label>
+      <slot />
+      {{ text }}
+    </label>
+  </template>
+</template>
