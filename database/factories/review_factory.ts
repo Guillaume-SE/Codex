389ed4521461cdf5c8ddc856d0e7ebdf084746ac
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2024-11-14 16:22:00 +0100

    chore: can now create many fake review for media

---
+++ database/factories/review_factory.ts
@@ -0,0 +1,21 @@
+import { MediaFactory } from '#database/factories/media_factory'
+import Review from '#models/review'
+import factory from '@adonisjs/lucid/factories'
+
+export const ReviewFactory = factory
+  .define(Review, async ({ faker }) => {
+    return {
+      rating:
+        faker.helpers.maybe(() => faker.number.int({ min: 0, max: 10 }), {
+          probability: 0.7,
+        }) ?? null,
+      opinion:
+        faker.helpers.maybe(() => faker.lorem.paragraph(), {
+          probability: 0.5,
+        }) ?? null,
+      // less probability = more chance to be false
+      isFavorite: faker.datatype.boolean({ probability: 0.3 }),
+    }
+  })
+  .relation('media', () => MediaFactory)
+  .build()
