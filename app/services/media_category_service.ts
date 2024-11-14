bf9828c7b7024d4b7adfcb35716a22d4c891d491
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2024-11-14 16:17:34 +0100

    chore: can now get genres and types related to a category

--- app/services/media_category_service.ts
+++ app/services/media_category_service.ts
@@ -1,4 +1,25 @@
+import Genre from '#models/genre'
+import MediaType from '#models/media_type'
+import type { MediaCategories } from '#types/MediaCategories'
 import { inject } from '@adonisjs/core'
 
 @inject()
-export default class MediaCategoryService {}
+export default class MediaCategoryService {
+  public async getCategoryTypes(category: MediaCategories) {
+    return await MediaType.query()
+      .select('id', 'name')
+      .whereHas('categories', (categoryQuery) => {
+        categoryQuery.where('name', category)
+      })
+      .orderBy('name')
+  }
+
+  public async getCategoryGenres(category: MediaCategories) {
+    return await Genre.query()
+      .select('id', 'name')
+      .whereHas('categories', (categoryQuery) => {
+        categoryQuery.where('name', category)
+      })
+      .orderBy('name')
+  }
+}
