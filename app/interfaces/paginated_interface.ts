0573d366d0077b966c468d0b07c24e77a78e0247
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2024-11-29 19:56:19 +0100

    chore: can now type paginated media

---
+++ app/interfaces/paginated_interface.ts
@@ -0,0 +1,14 @@
+export interface IPaginated<T> {
+  meta: {
+    total: number
+    perPage: number
+    currentPage: number
+    lastPage: number
+    firstPage: number
+    firstPageUrl: string
+    lastPageUrl: string
+    nextPageUrl: string | null
+    previousPageUrl: string | null
+  }
+  data: Array<T>
+}
