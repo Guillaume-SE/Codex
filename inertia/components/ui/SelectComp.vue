3c29f9d32c6306d4706f617c108a303c75e4f13b
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2024-12-23 14:29:56 +0100

    chore: can now use a "select" component

---
+++ inertia/components/ui/SelectComp.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const model = defineModel()
+
+defineProps<{
+  options: {
+    label: string
+    value: string | number
+    description?: string
+  }[]
+}>()
+</script>
+
+<template>
+  <select v-model="model">
+    <option v-for="option in options" :key="option.value" :value="option.value">
+      {{ option.label }}
+      <span v-if="option.description">
+        {{ option.description }}
+      </span>
+    </option>
+  </select>
+</template>
