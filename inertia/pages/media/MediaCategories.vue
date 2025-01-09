fdf75e2afba79b6db231007e54e485f9c0697301
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-01-09 16:05:19 +0100

    feat: can now see all categories available with some media recently added

--- inertia/pages/media/MediaCategories.vue
+++ inertia/pages/media/MediaCategories.vue
@@ -1,19 +1,36 @@
 <script setup lang="ts">
-import { Link } from '@inertiajs/vue3';
-import AppHead from '~/components/AppHead.vue';
-// import AppLayout from '~/layouts/AppLayout.vue';
+import type MediaController from '#controllers/media_controller'
+import { InferPageProps } from '@adonisjs/inertia/types'
+import { Link } from '@inertiajs/vue3'
+import AppHead from '~/components/AppHead.vue'
+import MediaCard from '~/components/MediaCard.vue'
 import AppLayout from '~/layouts/AppLayout.vue'
+
+defineProps<{
+  categoriesOverview: InferPageProps<MediaController, 'showCategories'>['categoriesOverview']
+}>()
 </script>
 
 <template>
   <AppHead title="Toutes les catégories" />
   <AppLayout>
-    <h2>Catégories.</h2>
-    <p>Toutes les catégories disponibles sur le site avec un aperçu des derniers ajouts</p>
-    <Link href="/category/game">Jeux</Link>
-    <Link href="/category/movie">Films</Link>
-    <Link href="/category/series">Séries</Link>
-    <Link href="/category/anime">Anime</Link>
-    <Link href="/category/book">Livres</Link>
+    <div>
+      <h3>Catégories</h3>
+      <p>L'ensemble des catégories disponible sur le site.</p>
+      <div v-for="category in categoriesOverview">
+        <div>
+          <h3>{{ category.label }}</h3>
+          <Link :href="`/category/${category.category}`"> Voir tout </Link>
+        </div>
+        <div class="recommandations-carousel">
+          <MediaCard
+            v-for="media in category.lastAddedList"
+            :key="media.id"
+            :media="media"
+            :mediaCategory="media.category"
+          />
+        </div>
+      </div>
+    </div>
   </AppLayout>
 </template>
