fe86e20815d314e3f448ee70514baf00e8cc3aba
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-01-11 11:43:15 +0100

    chore: added template for the dashboard panel

---
+++ inertia/pages/admin/Dashboard.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { Link } from '@inertiajs/vue3'
+import AppHead from '~/components/AppHead.vue'
+import AppLayout from '~/layouts/AppLayout.vue'
+</script>
+
+<template>
+  <AppHead title="Dashboard" />
+  <AppLayout>
+    <div>
+      <h3>Media</h3>
+      <Link href="/media/create">Ajouter un media</Link>
+    </div>
+  </AppLayout>
+</template>
