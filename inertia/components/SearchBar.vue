de7bbe3296b52a754c49ed97175ba12d3260df31
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-01-11 11:42:53 +0100

    chore: created a component for search bar

---
+++ inertia/components/SearchBar.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import ButtonComp from './ui/ButtonComp.vue'
+import InputComp from './ui/InputComp.vue'
+import LabelComp from './ui/LabelComp.vue'
+
+const model = defineModel<string>()
+
+defineProps<{
+  placeholder?: string
+}>()
+</script>
+
+<template>
+  <div>
+    <LabelComp text="Recherche" text-position="up">
+      <InputComp v-model="model" type="search" :placeholder="placeholder" />
+    </LabelComp>
+    <ButtonComp type="submit">Rechercher</ButtonComp>
+  </div>
+</template>
