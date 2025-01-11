0c7e143303a7ee139c9288814f250349c8a562ad
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-01-11 11:43:45 +0100

    chore: added template for create media form

---
+++ inertia/pages/admin/CreateMedia.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { useForm } from '@inertiajs/vue3'
+import AppHead from '~/components/AppHead.vue'
+import ButtonComp from '~/components/ui/ButtonComp.vue'
+import InputComp from '~/components/ui/InputComp.vue'
+import LabelComp from '~/components/ui/LabelComp.vue'
+import SelectComp from '~/components/ui/SelectComp.vue'
+import AppLayout from '~/layouts/AppLayout.vue'
+
+const newForm = useForm({
+  categoryId: null,
+  statusId: null,
+  typeId: null,
+  name: null,
+  alternativeName: null,
+  released: null,
+  synopsis: null,
+  tagId: null,
+  genreId: [],
+  contributors: [{ contributorId: null, roleId: null }],
+})
+</script>
+
+<template>
+  <AppHead title="Ajouter un media" />
+  <AppLayout>
+    <div>
+      <form method="POST" @submit.prevent="newForm.post('/media')"></form>
+    </div>
+  </AppLayout>
+</template>
