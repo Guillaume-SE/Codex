7fb3e2dd52773eadb5b517cc3af030cb6b6d0c08
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-02-17 12:14:06 +0100

    chore: added queries to get types and genres for categories

--- app/services/media_category_service.ts
+++ app/services/media_category_service.ts
@@ -2,6 +2,7 @@ import Genre from '#models/genre'
 import MediaType from '#models/media_type'
 import type { MediaCategories } from '#types/MediaCategories'
 import { inject } from '@adonisjs/core'
+import db from '@adonisjs/lucid/services/db'
 
 @inject()
 export default class MediaCategoryService {
@@ -20,6 +21,22 @@ export default class MediaCategoryService {
       .whereHas('categories', (categoryQuery) => {
         categoryQuery.where('name', category)
       })
+      .orderBy('name')
+  }
+
+  public async getCategoriesTypes() {
+    return await db
+      .from('category_types')
+      .join('media_types', 'category_types.type_id', 'media_types.id')
+      .select('category_types.category_id', 'media_types.id', 'media_types.name')
+      .orderBy('name')
+  }
+
+  public async getCategoriesGenres() {
+    return await db
+      .from('category_genres')
+      .join('genres', 'category_genres.genre_id', 'genres.id')
+      .select('category_genres.category_id', 'genres.id', 'genres.name')
       .orderBy('name')
   }
 }
