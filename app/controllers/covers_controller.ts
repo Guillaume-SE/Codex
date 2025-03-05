c625d847977cdbc0b8f0fd841b5ea8f763c388e0
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-03-05 18:06:54 +0100

    refactor: removed response status for inertia render

--- app/controllers/covers_controller.ts
+++ app/controllers/covers_controller.ts
@@ -1,50 +1,57 @@
+import { MediaPresenterFactory } from '#classes/MediaPresenter'
 import Cover from '#models/cover'
 import CoverService from '#services/cover_service'
-import { deleteCoverValidator, manageCoverValidator } from '#validators/cover_validator'
+import MediaService from '#services/media_service'
+import {
+  deleteCoverValidator,
+  manageCoverValidator,
+  mediaCoverValidator,
+} from '#validators/cover_validator'
 import { inject } from '@adonisjs/core'
 import type { HttpContext } from '@adonisjs/core/http'
 
 @inject()
 export default class CoversController {
-  constructor(readonly coverService: CoverService) {}
+  constructor(
+    readonly coverService: CoverService,
+    readonly mediaService: MediaService
+  ) {}
 
-  async manageOne({ params, response, request }: HttpContext) {
+  async showManage({ request, inertia }: HttpContext) {
+    const { params } = await request.validateUsing(mediaCoverValidator)
+    const media = await this.mediaService.getOne(params.mediaId)
+    const presentedMedia = MediaPresenterFactory.presentMedia(media)
+
+    return inertia.render('admin/ManageCover', {
+      media: presentedMedia,
+    })
+  }
+
+  async manageOne({ params, request, response }: HttpContext) {
     const mediaId = params.mediaId
-    try {
-      const { cover } = await request.validateUsing(manageCoverValidator)
-      const uploadedCover = await this.coverService.storeCover(cover)
-      await this.coverService.saveStoredCoverFilenames(uploadedCover, mediaId)
 
-      return response.status(201)
-    } catch (error) {
-      return response.status(404).json({ error, customError: error.message })
-    }
+    const { cover } = await request.validateUsing(manageCoverValidator)
+    const uploadedCover = await this.coverService.storeCover(cover)
+    await this.coverService.saveStoredCoverFilenames(uploadedCover, mediaId)
+
+    return response.redirect().toRoute('dashboard.home')
   }
 
   public async deleteOne({ request, params, response }: HttpContext) {
     const mediaId = params.mediaId
 
-    try {
-      await request.validateUsing(deleteCoverValidator)
-      const cover = await Cover.findByOrFail('media_id', mediaId)
+    await request.validateUsing(deleteCoverValidator)
+    const cover = await Cover.findByOrFail('media_id', mediaId)
 
-      await this.coverService.deleteCoverFile({
-        original: cover.originalCoverFilename,
-        small: cover.smallCoverFilename,
-        medium: cover.mediumCoverFilename,
-        large: cover.largeCoverFilename,
-      })
+    await this.coverService.deleteCoverFile({
+      original: cover.originalCoverFilename,
+      small: cover.smallCoverFilename,
+      medium: cover.mediumCoverFilename,
+      large: cover.largeCoverFilename,
+    })
 
-      await cover.delete()
+    await cover.delete()
 
-      return response.status(201)
-    } catch (error) {
-      return response.status(404).json({ error, customError: error.message })
-    }
-  }
-
-  public async show({ response }: HttpContext) {
-    const coverList = await Cover.all()
-    return response.status(201).json(coverList)
+    return response.redirect().toRoute('dashboard.home')
   }
 }
