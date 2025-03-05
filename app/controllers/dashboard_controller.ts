b44224e04cb02664607b2491b1aa57fe715a5ee6
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-03-05 18:07:37 +0100

    chore: can now render the media list

--- app/controllers/dashboard_controller.ts
+++ app/controllers/dashboard_controller.ts
@@ -1,9 +1,16 @@
+import { MediaPresenterFactory } from '#classes/MediaPresenter'
+import MediaService from '#services/media_service'
 import { inject } from '@adonisjs/core'
 import type { HttpContext } from '@adonisjs/core/http'
 
 @inject()
 export default class DashboardController {
   async showDashboard({ inertia }: HttpContext) {
-    return inertia.render('admin/Dashboard')
+    const mediaList = await MediaService.getAllPaginated()
+    const paginatedMediaList = MediaPresenterFactory.presentMediaList(mediaList)
+
+    return inertia.render('admin/Dashboard', {
+      mediaList: paginatedMediaList,
+    })
   }
 }
