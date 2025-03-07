b84b11eef6d1bcb73f3615dad56df0707c7dc977
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-03-07 18:54:43 +0100

    chore: added routes to get the manage review form

--- start/routes.ts
+++ start/routes.ts
@@ -30,6 +30,7 @@ router.put('/media/:mediaId', [MediaController, 'updateOne'])
 router.delete('/media/:mediaId', [MediaController, 'deleteOne'])
 
 //* reviews
+router.get('/media/:mediaId/review', [ReviewsController, 'showManage'])
 router.post('/media/:mediaId/review', [ReviewsController, 'manageReview'])
 router.put('/media/:mediaId/review', [ReviewsController, 'manageReview'])
 
