84fd675ea86af789b81a2c437ed04f5160fda085
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-03-07 18:55:07 +0100

    chore: added form to add and update a media review

---
+++ inertia/pages/admin/ManageReview.vue
@@ -0,0 +1,93 @@
+<script setup lang="ts">
+import type ReviewController from '#controllers/reviews_controller'
+import { InferPageProps } from '@adonisjs/inertia/types'
+import { useForm } from '@inertiajs/vue3'
+import { computed, onMounted, ref, watch } from 'vue'
+import AppHead from '~/components/AppHead.vue'
+import RatingBox from '~/components/RatingBox.vue'
+import ButtonComp from '~/components/ui/ButtonComp.vue'
+import InputComp from '~/components/ui/InputComp.vue'
+import LabelComp from '~/components/ui/LabelComp.vue'
+import AppLayout from '~/layouts/AppLayout.vue'
+
+const props = defineProps<{
+  media: InferPageProps<ReviewController, 'showManage'>['media']
+}>()
+
+interface IForm {
+  rating: string | number | null
+  opinion: string | null
+  isFavorite: boolean
+}
+
+const newForm = useForm<IForm>({
+  rating: '',
+  opinion: '',
+  isFavorite: false,
+})
+
+function submit() {
+  newForm.post(`/media/${props.media.id}/review`)
+}
+
+onMounted(() => {
+  if (props.media.review) {
+    newForm.rating = props.media.review.rating
+    newForm.opinion = props.media.review.opinion
+    newForm.isFavorite = props.media.review.isFavorite
+  }
+})
+
+const ratingValues = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+</script>
+
+<template>
+  <AppHead title="Gestion de review" />
+  <AppLayout>
+    <div>
+      <form @submit.prevent="submit">
+        <!-- rating -->
+        <div>
+          <span>Note:</span>
+          <div>
+            <LabelComp text="Aucune note" textPosition="down">
+              <InputComp v-model="newForm.rating" type="radio" />
+            </LabelComp>
+          </div>
+          <div v-for="rating in ratingValues">
+            <LabelComp textPosition="down">
+              <InputComp v-model="newForm.rating" type="radio" :value="rating" />
+              <RatingBox :rating="rating" />
+            </LabelComp>
+          </div>
+        </div>
+        <!-- opinion -->
+        <div>
+          <div>
+            <LabelComp text="Avis:" text-position="up" for="opinion">
+              <textarea
+                v-model="newForm.opinion"
+                placeholder="Très bon film, bande son incroyable..."
+                id="opinion"
+              ></textarea>
+            </LabelComp>
+          </div>
+        </div>
+        <!-- favoris -->
+        <div>
+          <div>
+            <LabelComp text="Mettre en coup de coeur" textPosition="down">
+              <InputComp
+                v-model="newForm.isFavorite"
+                type="checkbox"
+                :true-value="1"
+                :false-value="0"
+              />
+            </LabelComp>
+          </div>
+        </div>
+        <ButtonComp type="submit" :disabled="newForm.processing"></ButtonComp>
+      </form>
+    </div>
+  </AppLayout>
+</template>
