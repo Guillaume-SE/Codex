74d687264e692691307fbbc72116795ddb532742
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-03-21 12:33:17 +0100

    chor: added modal to confirm media delete

--- inertia/pages/admin/Dashboard.vue
+++ inertia/pages/admin/Dashboard.vue
@@ -1,16 +1,50 @@
 <script setup lang="ts">
 import type DashboardController from '#controllers/dashboard_controller'
 import { InferPageProps } from '@adonisjs/inertia/types'
-import { Link } from '@inertiajs/vue3'
+import { Link, useForm } from '@inertiajs/vue3'
+import { ref } from 'vue'
 import AppHead from '~/components/AppHead.vue'
 import ImageNotAvailableIcon from '~/components/icons/ImageNotAvailableIcon.vue'
 import RatingBox from '~/components/RatingBox.vue'
 import StatusProgressBadge from '~/components/StatusProgressBadge.vue'
+import ButtonComp from '~/components/ui/ButtonComp.vue'
+import ModalComp from '~/components/ui/ModalComp.vue'
 import AppLayout from '~/layouts/AppLayout.vue'
 
 defineProps<{
   mediaList: InferPageProps<DashboardController, 'showDashboard'>['mediaList']
 }>()
+
+interface IForm {
+  mediaId: number | null
+}
+
+const form = useForm<IForm>({
+  mediaId: null,
+})
+
+const showModal = ref<boolean>(false)
+const mediaTitle = ref<string>('')
+
+function submitDeleteMedia() {
+  const mediaId = form.mediaId
+  form.delete(`media/${mediaId}`, {
+    onSuccess: () => {
+      form.reset()
+    },
+  })
+  closeModal()
+}
+
+const openModal = (id: number, title: string) => {
+  form.mediaId = id
+  mediaTitle.value = title
+  showModal.value = true
+}
+const closeModal = () => {
+  showModal.value = false
+  form.mediaId = null
+}
 </script>
 
 <template>
@@ -23,6 +57,9 @@ defineProps<{
 
     <div class="dashboard-list">
       <div v-for="media in mediaList" class="dashboard-list-item">
+        <div>
+          <input v-model="form.mediaId" type="checkbox" :value="media.id" />
+        </div>
         <div v-if="media.cover" class="dashboard-list_img">
           <Link :href="`/media/${media.id}/cover`">
             <img
@@ -58,7 +95,50 @@ defineProps<{
             :class="'status-progress-text-small status-progress-container-small'"
           />
         </div>
+        <div>
+          <div>
+            <ButtonComp @click="openModal(media.id, media.name)">Supprimer</ButtonComp>
+          </div>
+        </div>
       </div>
+      <ModalComp :show="showModal" @close-modal="closeModal">
+        <template #header>
+          <div>
+            <span> Confirmation </span>
+          </div>
+        </template>
+        <template #content>
+          <div>
+            <span> Confirmer la suppression de {{ mediaTitle }} ? </span>
+          </div>
+        </template>
+        <template #action>
+          <ButtonComp @click="closeModal">Retour</ButtonComp>
+          <ButtonComp @click="submitDeleteMedia">Confirmer</ButtonComp>
+        </template>
+      </ModalComp>
     </div>
   </AppLayout>
 </template>
+
+<style scoped>
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.4);
+  z-index: 1;
+}
+.modal {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 2;
+  background: white;
+  padding: 20px;
+  border-radius: 8px;
+}
+</style>
