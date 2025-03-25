013785d381ceaad25a101a4145a94542063eba7a
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-03-25 15:57:36 +0100

    feat: added pagination to the dashboard results

--- inertia/pages/admin/Dashboard.vue
+++ inertia/pages/admin/Dashboard.vue
@@ -5,25 +5,32 @@ import { Link, useForm } from '@inertiajs/vue3'
 import { ref, useTemplateRef } from 'vue'
 import AppHead from '~/components/AppHead.vue'
 import ImageNotAvailableIcon from '~/components/icons/ImageNotAvailableIcon.vue'
+import Pagination from '~/components/Pagination.vue'
 import RatingBox from '~/components/RatingBox.vue'
+import SearchBar from '~/components/SearchBar.vue'
 import StatusProgressBadge from '~/components/StatusProgressBadge.vue'
 import ButtonComp from '~/components/ui/ButtonComp.vue'
 import ModalComp from '~/components/ui/ModalComp.vue'
 import AppLayout from '~/layouts/AppLayout.vue'
 
-defineProps<{
+const props = defineProps<{
   mediaList: InferPageProps<DashboardController, 'showDashboard'>['mediaList']
 }>()
 
 interface IForm {
   mediaId: number | null
 }
+interface IFilters {
+  search: string
+}
 
 const form = useForm<IForm>({
   mediaId: null,
 })
+const filters = useForm<IFilters>('filterResults', {
+  search: '',
+})
 
-// const showModal = ref<boolean>(false)
 const mediaTitle = ref<string>('')
 const modalRef = useTemplateRef<HTMLDialogElement>('modalRef')
 
@@ -36,6 +43,9 @@ function submitDeleteMedia() {
   })
   closeModal()
 }
+function submitFilters() {
+  filters.get('/dashboard', { preserveState: true })
+}
 
 const openModal = (id: number, title: string) => {
   form.mediaId = id
@@ -46,21 +56,32 @@ const closeModal = () => {
   form.mediaId = null
   modalRef.value?.close()
 }
+
+// paginate with filters included
+function fetchNewPageData(url: string | null) {
+  filters.get(`${url}`, { preserveState: true })
+}
 </script>
 
 <template>
   <AppHead title="Dashboard" />
   <AppLayout>
     <div>
-      <h3>Media</h3>
       <Link href="/media/manage">Ajouter un media</Link>
     </div>
 
     <div class="dashboard-list">
-      <div v-for="media in mediaList" class="dashboard-list-item">
+      <form action="GET" @submit.prevent="submitFilters">
+        <div>
+          <!-- search -->
+          <SearchBar v-model="filters.search" />
+        </div>
+      </form>
+      <div v-for="media in mediaList.data" class="dashboard-list-item">
         <div>
           <input type="checkbox" :value="media.id" />
         </div>
+        <!-- cover -->
         <div v-if="media.cover" class="dashboard-list_img">
           <Link :href="`/media/${media.id}/cover`">
             <img
@@ -79,17 +100,19 @@ const closeModal = () => {
             </div>
           </Link>
         </div>
-
+        <!-- name -->
         <div>
           <Link :href="`/media/manage/${media.id}`">
             <p>{{ media.name }}</p>
           </Link>
         </div>
+        <!-- review rating -->
         <div>
           <Link :href="`/media/${media.id}/review`">
             <RatingBox :rating="media.review?.rating ? media.review.rating : null" />
           </Link>
         </div>
+        <!-- status progress -->
         <div>
           <StatusProgressBadge
             :status="media.status"
@@ -102,6 +125,26 @@ const closeModal = () => {
           </div>
         </div>
       </div>
+
+      <!-- pagination -->
+      <div>
+        <Pagination
+          :page="{
+            currentPage: props.mediaList.meta.currentPage,
+            firstPage: props.mediaList.meta.firstPage,
+            lastPage: props.mediaList.meta.lastPage,
+          }"
+          :url="{
+            firstPageUrl: props.mediaList.meta.firstPageUrl,
+            lastPageUrl: props.mediaList.meta.lastPageUrl,
+            nextPageUrl: props.mediaList.meta.nextPageUrl,
+            previousPageUrl: props.mediaList.meta.previousPageUrl,
+          }"
+          @update:current-page="fetchNewPageData"
+        />
+      </div>
+
+      <!-- delete modal -->
       <ModalComp ref="modalRef" @close-modal="closeModal">
         <template #header>
           <div>
