b53c03e5a1e140c3b87363777864a9f74178dec8
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-03-31 16:07:15 +0200

    chore: added composable to reuse date format in templates

--- inertia/composables/useFormattedDate.ts
+++ inertia/composables/useFormattedDate.ts
@@ -1,8 +1,18 @@
-import { formatDate } from '#utils/formatDate'
+import { formatDate, formatDateToLocale } from '#utils/formatDate'
 
 export function useFormattedDate(date: string | null) {
   if (date) {
     return formatDate(date)
   }
   return ''
+}
+
+export function useFormattedDateToLocale(
+  isoDate: string | null | number,
+  withTime: boolean = false
+) {
+  if (!isoDate) {
+    return
+  }
+  return formatDateToLocale(isoDate, withTime)
 }
