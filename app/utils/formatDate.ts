e7c2ecfff75fdc9ade608f2a4948157d44969e4d
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-03-31 16:08:56 +0200

    chore: added function to format a date in readable value for users

--- app/utils/formatDate.ts
+++ app/utils/formatDate.ts
@@ -6,3 +6,20 @@ export function formatDate(isoDate: string) {
   const day = String(date.getDate()).padStart(2, '0')
   return `${year}-${month}-${day}`
 }
+
+export function formatDateToLocale(isoDate: string | number, withTime: boolean = false) {
+  const date = new Date(isoDate)
+  const formattedDate = date.toLocaleDateString()
+
+  if (withTime) {
+    const formattedTime = date.toLocaleTimeString(undefined, {
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: false,
+    })
+
+    return `${formattedDate} à ${formattedTime}`
+  }
+
+  return formattedDate
+}
