925a8b8209f0390cc29462c9cfbcef2ab5380d12
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-06-13 15:46:47 +0200

    chore: added logic to replace and delete a type that are used by media

--- app/controllers/media_types_controller.ts
+++ app/controllers/media_types_controller.ts
@@ -2,20 +2,36 @@ import MediaType from '#models/media_type'
 import MediaTypeService from '#services/media_type_service'
 import {
   createMediaTypeValidator,
+  replaceMediaTypeValidator,
+  singleMediaTypeValidator,
   updateMediaTypeValidator,
 } from '#validators/media_type_validator'
 import { inject } from '@adonisjs/core'
 import type { HttpContext } from '@adonisjs/core/http'
+
+interface ITypeListFormatted {
+  id: number
+  name: string
+  count: number
+}
 
 @inject()
 export default class MediaTypesController {
   constructor(protected mediaTypeService: MediaTypeService) {}
 
   async showManage({ inertia }: HttpContext) {
-    const typeList: MediaType[] = await MediaType.query().orderBy('name', 'asc')
+    const typeList: MediaType[] = await MediaType.query().withCount('media').orderBy('name', 'asc')
+
+    const typeListFormatted: ITypeListFormatted[] = typeList.map((type) => {
+      return {
+        id: type.id,
+        name: type.name,
+        count: type.$extras.media_count,
+      }
+    })
 
     return inertia.render('admin/ManageType', {
-      typeList,
+      typeList: typeListFormatted,
     })
   }
 
@@ -29,6 +45,20 @@ export default class MediaTypesController {
     // for create
     const data = await request.validateUsing(createMediaTypeValidator)
     await this.mediaTypeService.storeOrUpdate(data)
+
+    return response.redirect().toRoute('type.manage')
+  }
+
+  public async replaceOne({ request, response }: HttpContext) {
+    const { params, newTypeId } = await request.validateUsing(replaceMediaTypeValidator)
+    await this.mediaTypeService.replaceAndDelete(params.typeId, newTypeId)
+
+    return response.redirect().toRoute('type.manage')
+  }
+
+  public async deleteOne({ request, response }: HttpContext) {
+    const { params } = await request.validateUsing(singleMediaTypeValidator)
+    await this.mediaTypeService.delete(params.typeId)
 
     return response.redirect().toRoute('type.manage')
   }
--- app/services/media_type_service.ts
+++ app/services/media_type_service.ts
@@ -1,3 +1,4 @@
+import Media from '#models/media'
 import MediaType from '#models/media_type'
 import { updateMediaTypeValidator } from '#validators/media_type_validator'
 import { inject } from '@adonisjs/core'
@@ -13,5 +14,30 @@ export default class MediaTypeService {
       type = await MediaType.findOrFail(typeId)
     }
     await type.merge(data).save()
+  }
+
+  public async replaceAndDelete(oldTypeId: number, newTypeId: number) {
+    const oldType = await MediaType.findOrFail(oldTypeId)
+    const newType = await MediaType.findOrFail(newTypeId)
+
+    const mediaUsingOldType = await Media.query().where('type_id', '=', oldType.id)
+
+    for (let media of mediaUsingOldType) {
+      media.typeId = newType.id
+      await media.save()
+    }
+
+    await oldType.delete()
+  }
+
+  public async delete(typeId: number) {
+    const type = await MediaType.findOrFail(typeId)
+    const hasMediaUsingThisType = await Media.findBy('typeId', type.id)
+
+    if (hasMediaUsingThisType) {
+      throw new Error('Le type est utilisé par au moins 1 media, suppression impossible')
+    }
+
+    await type.delete()
   }
 }
