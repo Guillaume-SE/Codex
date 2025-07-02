d884b534acc68cd490a4c9939b67eb9c4e6d6360
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-07-02 21:33:43 +0200

    chore: added a layout for the admin panel

---
+++ inertia/layouts/DashboardLayout.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { Link } from '@inertiajs/vue3'
+</script>
+
+<template>
+  <main>
+    <header>
+      <nav class="navbar">
+        <Link href="/dashboard">Général</Link>
+        <Link href="/platform/manage">Plateformes</Link>
+        <Link href="/genre/manage">Genres</Link>
+        <Link href="/type/manage">Types</Link>
+        <Link href="/category/manage">Catégories</Link>
+      </nav>
+    </header>
+    <div>
+      <slot />
+    </div>
+  </main>
+</template>
