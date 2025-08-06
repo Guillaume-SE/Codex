48f50920caf996098759e998cc983bfd372a47fa
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-08-06 17:02:08 +0200

    refactor: minor looking change for not found page

--- inertia/pages/errors/NotFound.vue
+++ inertia/pages/errors/NotFound.vue
@@ -5,13 +5,11 @@ import AppLayout from '~/layouts/AppLayout.vue'
 </script>
 
 <template>
-  <AppHead title="404 Not Found" />
+  <AppHead title="Page introuvable" />
   <AppLayout>
     <div>
       <h3>Oups une 404 !</h3>
-      <p>La page que vous avez recherché est malheureusement introuvable !</p>
-      <p>Soit elle a été supprimée, soit le lien comporte une erreur.</p>
-      <p>Si c'est une recherche spécifique, vérifiez son orthographe.</p>
+      <p>La page recherchée est introuvable !</p>
       <Link href="/">Retour à l'accueil</Link>
     </div>
   </AppLayout>
