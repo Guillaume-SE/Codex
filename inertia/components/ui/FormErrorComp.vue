83dde0dedc6ac5c9cd76482710d017b010eedc5e
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-08-07 20:00:18 +0200

    chore: added comp to show error message

---
+++ inertia/components/ui/FormErrorComp.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{
+  message: string[]
+}>()
+</script>
+
+<template>
+  <div v-for="error in message">
+    <span class="form-text-error">{{ error }}</span>
+  </div>
+</template>
+
+<style scoped>
+.form-text-error {
+  color: #fa2d36;
+  font-weight: 500;
+}
+</style>
