c7f15bd313a182fcc66df5ad229ea7864a67b05f
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-08-07 19:45:07 +0200

    chore: added toast notification

--- inertia/layouts/AppLayout.vue
+++ inertia/layouts/AppLayout.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts">
 import { Link } from '@inertiajs/vue3'
+import ToastNotification from '~/components/ui/ToastNotification.vue'
+
+// const page = usePage()
+// const message = computed(() => page.props.success)
+
+defineProps<{
+  success?: string
+}>()
 </script>
 
 <template>
@@ -9,10 +17,14 @@ import { Link } from '@inertiajs/vue3'
         <Link href="/">Accueil</Link>
         <Link href="/categories">Catégories</Link>
         <Link href="/dashboard">Dashboard</Link>
+        <Link href="/categories/game">Jeux</Link>
+        <Link href="/categories/movie">Films</Link>
+        <Link href="/categories/series">Séries</Link>
+        <Link href="/categories/anime">Anime</Link>
+        <Link href="/categories/book">Livres</Link>
       </nav>
     </header>
-    <div>
-      <slot />
-    </div>
+    <slot />
+    <ToastNotification v-if="success" :message="success" />
   </main>
 </template>
