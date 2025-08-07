bbb6ee85be55f50a13d7a28a774d3c72c3062d47
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-08-07 19:48:58 +0200

    chore: status now have a dynamic background color

--- inertia/components/StatusProgressBadge.vue
+++ inertia/components/StatusProgressBadge.vue
@@ -1,20 +1,84 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 import StatusProgressIcon from '~/components/icons/StatusProgressIcon.vue'
-import { useStatusIconColorClass } from '~/composables/useStatusIconColorClass'
+import { useStatusBackgroundColor } from '~/composables/useStatusBackgroundColor'
+import { useStatusIconColor } from '~/composables/useStatusIconColor'
 
 const props = defineProps<{
   status: string
 }>()
 
-const ratingColor = computed(() => useStatusIconColorClass(props.status))
+const ratingColor = computed(() => useStatusIconColor(props.status))
+const ratingBackground = computed(() => useStatusBackgroundColor(props.status))
 </script>
 
 <template>
-  <div class="status-progress-container">
+  <div :class="[ratingBackground, 'status-progress-container']">
     <StatusProgressIcon :class="ratingColor" />
     <span class="status-progress-text">
       {{ status }}
     </span>
   </div>
 </template>
+
+<style scoped>
+.status-progress-container {
+  display: flex;
+  align-items: center;
+  padding: 5px 10px;
+  width: 75px;
+  border-radius: 10px;
+}
+
+.status-progress-text {
+  /* color: #fafafa; */
+  color: #0f172a;
+  font-weight: 700;
+  font-size: 0.8rem;
+}
+
+.status-progress-icon {
+  width: 12px;
+  height: 12px;
+  margin-right: 5px;
+  border-radius: 50%;
+}
+
+.icon-in-progress {
+  background-color: #2c7fff;
+}
+.icon-completed {
+  background-color: #00bd7c;
+}
+.icon-paused {
+  background-color: #f1b000;
+}
+.icon-dropped {
+  background-color: #ff1f56;
+}
+.icon-planned {
+  background-color: #79716b;
+}
+.icon-anticipated {
+  background-color: #ad47ff;
+}
+
+.background-in-progress {
+  background-color: #daebfe;
+}
+.background-completed {
+  background-color: #d0fae5;
+}
+.background-paused {
+  background-color: #fef9c1;
+}
+.background-dropped {
+  background-color: #ffe2e1;
+}
+.background-planned {
+  background-color: #f5f5f4;
+}
+.background-anticipated {
+  background-color: #f3e8ff;
+}
+</style>
