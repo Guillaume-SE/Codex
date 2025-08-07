e1ada229ef206c9ce5246eaafc2ee39ca1299f83
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-08-07 20:00:40 +0200

    chore: added comp to show success message

---
+++ inertia/components/ui/ToastNotification.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps<{
+  message: string
+}>()
+</script>
+
+<template>
+  <div class="toast-container">
+    <span class="form-text-success">{{ message }}</span>
+  </div>
+</template>
+
+<style scoped>
+.toast-container {
+  background-color: #f1fdf3;
+}
+.form-text-success {
+  color: #008235;
+  font-weight: 500;
+}
+</style>
