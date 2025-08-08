758647b5625eb9f999397ac29a9a46dd3debe0d3
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-08-08 19:53:03 +0200

    chore: added component for action in dashboard

---
+++ inertia/components/DashboardAction.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { Link } from '@inertiajs/vue3'
+import SearchBar from '~/components/SearchBar.vue'
+
+const model = defineModel<string>('search')
+
+defineProps<{
+  type: 'search' | 'select'
+}>()
+</script>
+
+<template>
+  <template v-if="type === 'search'">
+    <div class="dashboard-action">
+      <SearchBar v-model="model" placeholder="Rechercher" />
+      <slot />
+    </div>
+  </template>
+</template>
+
+<style scoped>
+.dashboard-action {
+  display: flex;
+  justify-content: end;
+}
+</style>
