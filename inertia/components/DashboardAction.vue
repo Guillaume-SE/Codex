e9cceeeaaf1351f9a681eef2fc8c82e30e60f52c
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-08-09 15:54:25 +0200

    refactor: added title

--- inertia/components/DashboardAction.vue
+++ inertia/components/DashboardAction.vue
@@ -6,19 +6,30 @@ const model = defineModel<string>('search')
 
 defineProps<{
   type: 'search' | 'select'
+  title: string
 }>()
 </script>
 
 <template>
   <template v-if="type === 'search'">
-    <div class="dashboard-action">
-      <SearchBar v-model="model" placeholder="Rechercher" />
-      <slot />
+    <div class="dashboard-title-container">
+      <div>
+        <h3>{{ title }}</h3>
+      </div>
+      <div class="dashboard-action">
+        <SearchBar v-model="model" placeholder="Rechercher" />
+        <slot />
+      </div>
     </div>
   </template>
 </template>
 
 <style scoped>
+.dashboard-title-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 .dashboard-action {
   display: flex;
   justify-content: end;
