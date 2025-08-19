0c18e28db142379972fdbb520d8ac298cf33e29d
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-08-19 18:29:33 +0200

    reafactor: added one location to handle status dynamic style

---
+++ inertia/composables/useStatusStyle.ts
@@ -0,0 +1,25 @@
+import type { MediaStatuses } from '#types/MediaStatuses'
+import { computed, toRef, type MaybeRef } from 'vue'
+
+const statusStyles: Record<MediaStatuses, { bg: string; icon: string }> = {
+  'en cours': { bg: 'background-in-progress', icon: 'icon-in-progress' },
+  'terminé': { bg: 'background-completed', icon: 'icon-completed' },
+  'en pause': { bg: 'background-paused', icon: 'icon-paused' },
+  'abandonné': { bg: 'background-dropped', icon: 'icon-dropped' },
+  'prévu': { bg: 'background-planned', icon: 'icon-planned' },
+  'attendu': { bg: 'background-anticipated', icon: 'icon-anticipated' },
+}
+
+export function useStatusStyles(status: MaybeRef<string>) {
+  const statusRef = toRef(status)
+
+  const backgroundColor = computed(() => {
+    return statusStyles[statusRef.value as MediaStatuses]?.bg || ''
+  })
+
+  const iconColor = computed(() => {
+    return statusStyles[statusRef.value as MediaStatuses]?.icon || ''
+  })
+
+  return { backgroundColor, iconColor }
+}
