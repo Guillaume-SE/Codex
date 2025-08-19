b9234404653b873aa20a5e2780897c81bfac7261
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-08-19 17:28:43 +0200

    refactor: can now take simple message or array of messages

--- inertia/components/ui/FormErrorComp.vue
+++ inertia/components/ui/FormErrorComp.vue
@@ -1,12 +1,24 @@
 <script setup lang="ts">
-defineProps<{
-  message: string
+import { computed } from 'vue'
+
+const props = defineProps<{
+  message: string[] | string
 }>()
+
+const displayMessage = computed(() => {
+  if (Array.isArray(props.message) && props.message.length > 0) {
+    return props.message[0]
+  }
+  if (typeof props.message === 'string') {
+    return props.message
+  }
+  return null
+})
 </script>
 
 <template>
-  <div>
-    <span class="form-text-error">{{ message }}</span>
+  <div v-if="displayMessage" class="form-text-error">
+    <span>{{ displayMessage }}</span>
   </div>
 </template>
 
