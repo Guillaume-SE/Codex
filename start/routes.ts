12a56b0abe24978a016df53251b758961a31d5ab
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-08-22 15:42:45 +0200

    refactor: added conventionnal naming

--- start/routes.ts
+++ start/routes.ts
@@ -18,59 +18,73 @@ router.where('categoryId', router.matchers.number())
 router.where('typeId', router.matchers.number())
 router.where('genreId', router.matchers.number())
 
-router.get('/', [HomeController, 'showHome']).as('home')
+router.group(() => {
+  router.get('/', [HomeController, 'showHome']).as('home')
+  router.get('/storage/*', [StorageController, 'show'])
+  router.get('/users/:id', [UsersController, 'show']).as('users.show')
 
-router.get('/storage/*', [StorageController, 'show'])
+  // Public media browsing routes
+  router.get('/categories', [MediaController, 'showCategories']).as('categories.index')
+  router
+    .group(() => {
+      router
+        .get('/categories/:categoryName', [MediaController, 'showByCategory'])
+        .as('categories.show')
+      router
+        .get('/categories/:categoryName/:mediaId', [MediaController, 'showOne'])
+        .as('media.show')
+    })
+    .where('categoryName', { match: /^(game|book|anime|series|movie)$/ })
+})
 
-//* users
-router.get('/users/:id', [UsersController, 'show'])
-
-//* media
-router.get('/categories', [MediaController, 'showCategories'])
+// ADMIN
 router
-  .get('/categories/:categoryName', [MediaController, 'showByCategory'])
-  .where('categoryName', { match: /^(game|book|anime|series|movie)$/ })
-router
-  .get('/categories/:categoryName/:mediaId', [MediaController, 'showOne'])
-  .where('categoryName', { match: /^(game|book|anime|series|movie)$/ })
+  .group(() => {
+    router.get('/dashboard', [DashboardController, 'showDashboard']).as('dashboard.home')
 
-//ADMIN
-router.get('/media/manage/:mediaId?', [MediaController, 'showManage'])
-router.post('/media', [MediaController, 'manageOne'])
-router.put('/media/:mediaId', [MediaController, 'manageOne'])
-router.delete('/media/:mediaId', [MediaController, 'deleteOne'])
+    // --- Media CRUD ---
+    router.get('/media/create', [MediaController, 'showManage']).as('media.create')
+    router.get('/media/:mediaId/edit', [MediaController, 'showManage']).as('media.edit')
 
-//* reviews
-router.get('/media/:mediaId/review', [ReviewsController, 'showManage'])
-router.post('/media/:mediaId/review', [ReviewsController, 'manageOne'])
+    router.post('/media', [MediaController, 'store']).as('media.store')
+    router.put('/media/:mediaId', [MediaController, 'update']).as('media.update')
+    router.delete('/media/:mediaId', [MediaController, 'destroy']).as('media.destroy')
 
-//* covers
-router.get('/media/:mediaId/cover', [CoversController, 'showManage'])
-router.post('/media/:mediaId/cover', [CoversController, 'manageOne'])
-router.delete('/media/:mediaId/cover', [CoversController, 'deleteOne'])
+    // Reviews
+    router.get('/media/:mediaId/review', [ReviewsController, 'showManage']).as('reviews.manage')
+    router.post('/media/:mediaId/review', [ReviewsController, 'store']).as('reviews.store')
+    router.put('/media/:mediaId/review', [ReviewsController, 'update']).as('reviews.update')
 
-//* categories
-router.get('/category/manage', [MediaCategoriesController, 'showManage']).as('category.manage')
-router.post('/category/:categoryId/associate', [MediaCategoriesController, 'manageAssociation'])
+    // Covers
+    router.get('/media/:mediaId/cover', [CoversController, 'showManage']).as('covers.manage')
+    router.post('/media/:mediaId/cover', [CoversController, 'manageOne']).as('covers.store')
+    router.delete('/media/:mediaId/cover', [CoversController, 'deleteOne']).as('covers.destroy')
 
-//* genres
-router.get('/genre/manage', [GenresController, 'showManage']).as('genre.manage')
-router.post('/genre', [GenresController, 'storeOrUpdate'])
-router.put('/genre/:genreId', [GenresController, 'storeOrUpdate'])
-router.delete('/genre/:genreId', [GenresController, 'deleteOne'])
+    // --- Other Management Pages (Genres, Types, Platforms) ---
+    router.get('/genres/manage', [GenresController, 'showManage']).as('genres.index')
+    router.post('/genres', [GenresController, 'store']).as('genres.store')
+    router.put('/genres/:genreId', [GenresController, 'update']).as('genres.update')
+    router.delete('/genres/:genreId', [GenresController, 'destroy']).as('genres.destroy')
 
-//* types
-router.get('/type/manage', [MediaTypesController, 'showManage']).as('type.manage')
-router.post('/type', [MediaTypesController, 'storeOrUpdate'])
-router.put('/type/:typeId', [MediaTypesController, 'storeOrUpdate'])
-router.put('/type/replace/:typeId', [MediaTypesController, 'replaceOne'])
-router.delete('/type/:typeId', [MediaTypesController, 'deleteOne'])
+    router.get('/types/manage', [MediaTypesController, 'showManage']).as('types.index')
+    router.post('/types', [MediaTypesController, 'store']).as('types.store')
+    router.put('/types/:typeId', [MediaTypesController, 'update']).as('types.update')
+    router.put('/types/replace/:typeId', [MediaTypesController, 'replaceOne']).as('types.replace')
+    router.delete('/types/:typeId', [MediaTypesController, 'destroy']).as('types.destroy')
 
-//* game platforms
-router.get('/platform/manage', [GamePlatformsController, 'showManage']).as('platform.manage')
-router.post('/platform', [GamePlatformsController, 'storeOrUpdate'])
-router.put('/platform/:platformId', [GamePlatformsController, 'storeOrUpdate'])
-router.delete('/platform/:platformId', [GamePlatformsController, 'deleteOne'])
+    router.get('/platforms/manage', [GamePlatformsController, 'showManage']).as('platforms.index')
+    router.post('/platforms', [GamePlatformsController, 'store']).as('platforms.store')
+    router.put('/platforms/:platformId', [GamePlatformsController, 'update']).as('platforms.update')
+    router
+      .delete('/platforms/:platformId', [GamePlatformsController, 'destroy'])
+      .as('platforms.destroy')
 
-//* dashboard
-router.get('/dashboard', [DashboardController, 'showDashboard']).as('dashboard.home')
+    router
+      .get('/categories/manage', [MediaCategoriesController, 'showManage'])
+      .as('categories.manage')
+    router
+      .post('/categories/:categoryId/associate', [MediaCategoriesController, 'associate'])
+      .as('categories.associate')
+  })
+  .prefix('/admin')
+// .use(middleware.auth())
