0db851a9b5c5c25c08439fb20c0d1dcf813c1620
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-08-22 15:53:34 +0200

    chore: added componenent to conditionaly displaying media details

---
+++ inertia/components/media/MediaSpecificDetails.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import type { IMediaPresented } from '#interfaces/media_presented_interface'
+import { computed, type PropType } from 'vue'
+import { useFormattedDuration } from '~/composables/useFormattedDuration'
+
+const props = defineProps({
+  media: {
+    type: Object as PropType<IMediaPresented>,
+    required: true,
+  },
+})
+
+const formattedDuration = computed(() =>
+  props.media.movieInfos ? useFormattedDuration(props.media.movieInfos.duration) : null
+)
+</script>
+
+<template>
+  <p v-if="media.gameInfos">Joué sur: {{ media.gameInfos.platform || 'N/A' }}</p>
+
+  <p v-if="media.movieInfos">Durée: {{ formattedDuration || 'N/A' }}</p>
+
+  <p v-if="media.seriesInfos">Nombre d'épisodes: {{ media.seriesInfos.seasonLength || 'N/A' }}</p>
+
+  <p v-if="media.animeInfos">Nombre d'épisodes: {{ media.animeInfos.seasonLength || 'N/A' }}</p>
+
+  <p v-if="media.bookInfos">Pages: {{ media.bookInfos.pages || 'N/A' }}</p>
+</template>
