dd504caf7ee488643ed4bfe07c5c026bd7c9e862
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-08-22 16:00:43 +0200

    refactor: use the built in preload methods

--- app/controllers/media_categories_controller.ts
+++ app/controllers/media_categories_controller.ts
@@ -2,71 +2,42 @@ import Genre from '#models/genre'
 import MediaCategory from '#models/media_category'
 import MediaType from '#models/media_type'
 import MediaCategoryService from '#services/media_category_service'
-import { typesAndGenresAssociationValidator } from '#validators/media_category_validator'
+import { categoryAssociationValidator } from '#validators/media_category_validator'
 import { inject } from '@adonisjs/core'
 import { HttpContext } from '@adonisjs/core/http'
-
-interface ICategoriesType {
-  category_id: number
-  type_id: number
-  name: string
-}
-interface ICategoriesGenre {
-  category_id: number
-  genre_id: number
-  name: string
-}
 
 @inject()
 export default class MediaCategoriesController {
   constructor(readonly mediaCategoryService: MediaCategoryService) {}
 
   public async showManage({ inertia }: HttpContext) {
-    const categoriesList = await MediaCategory.query().orderBy('name', 'asc')
     const typesList = await MediaType.query().orderBy('name', 'asc')
     const genresList = await Genre.query().orderBy('name', 'asc')
-    const categoriesTypes = await this.mediaCategoryService.getCategoriesTypes()
-    const categoriesGenres = await this.mediaCategoryService.getCategoriesGenres()
+    const categories = await MediaCategory.query()
+      .preload('types')
+      .preload('genres')
+      .orderBy('name', 'asc')
 
-    const mapCategoryAssociations = (
-      categoriesTypes: ICategoriesType[],
-      categoriesGenres: ICategoriesGenre[]
-    ) => {
-      const result: Record<string, { types: number[]; genres: number[] }> = {}
-
-      const ensureCategory = (categoryId: number | string) => {
-        const key = String(categoryId)
-        if (!result[key]) {
-          result[key] = { types: [], genres: [] }
-        }
-        return result[key]
+    const presentedCategories = categories.map((category) => {
+      return {
+        id: category.id,
+        name: category.name,
+        typeIds: category.types.map((t) => t.id),
+        genreIds: category.genres.map((g) => g.id),
       }
-
-      for (const { category_id, type_id } of categoriesTypes) {
-        ensureCategory(category_id).types.push(type_id)
-      }
-
-      for (const { category_id, genre_id } of categoriesGenres) {
-        ensureCategory(category_id).genres.push(genre_id)
-      }
-
-      return result
-    }
-
-    const categoriesTypesGenresPaired = mapCategoryAssociations(categoriesTypes, categoriesGenres)
+    })
 
     return inertia.render('admin/ManageCategoryAssociation', {
-      categoriesList,
+      categories: presentedCategories,
       typesList,
       genresList,
-      categoriesTypesGenresPaired,
     })
   }
 
-  public async manageAssociation({ response, request }: HttpContext) {
-    const { params, ...data } = await request.validateUsing(typesAndGenresAssociationValidator)
-    await this.mediaCategoryService.associateTypesAndGenres(data, params.categoryId)
+  public async associate({ params, response, request }: HttpContext) {
+    const data = await request.validateUsing(categoryAssociationValidator)
+    await this.mediaCategoryService.associate(data, params.categoryId)
 
-    return response.redirect().toRoute('category.manage')
+    return response.redirect().toRoute('categories.manage')
   }
 }
