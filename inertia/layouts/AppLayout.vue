cb64b39a7720fe8a558b34d9bfe3bee69efd7d3d
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-09-30 18:42:35 +0200

    refactor: now use ToastManager

--- inertia/layouts/AppLayout.vue
+++ inertia/layouts/AppLayout.vue
@@ -1,27 +1,28 @@
 <script setup lang="ts">
 import { Link } from '@inertiajs/vue3'
-import ToastNotification from '~/components/ui/ToastNotification.vue'
+import ToastManager from '~/components/ToastManager.vue'
 
 defineProps<{
   success?: string
+  error?: string
 }>()
 </script>
 
 <template>
+  <header>
+    <nav class="navbar">
+      <Link href="/">Accueil</Link>
+      <Link href="/categories">Catégories</Link>
+      <Link href="/admin/dashboard">Dashboard</Link>
+      <Link href="/categories/game">Jeux</Link>
+      <Link href="/categories/movie">Films</Link>
+      <Link href="/categories/series">Séries</Link>
+      <Link href="/categories/anime">Anime</Link>
+      <Link href="/categories/book">Livres</Link>
+    </nav>
+  </header>
   <main>
-    <header>
-      <nav class="navbar">
-        <Link href="/">Accueil</Link>
-        <Link href="/categories">Catégories</Link>
-        <Link href="/admin/dashboard">Dashboard</Link>
-        <Link href="/categories/game">Jeux</Link>
-        <Link href="/categories/movie">Films</Link>
-        <Link href="/categories/series">Séries</Link>
-        <Link href="/categories/anime">Anime</Link>
-        <Link href="/categories/book">Livres</Link>
-      </nav>
-    </header>
     <slot />
-    <ToastNotification v-if="success" :message="success" />
   </main>
+  <ToastManager :success="success" :error="error" />
 </template>
