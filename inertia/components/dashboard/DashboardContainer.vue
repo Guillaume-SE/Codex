3f5d4ecb1ed9499ff37f2d4a06b78f6a1f6ba043
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-10-02 15:33:07 +0200

    chore: added dashboard categories navigation

---
+++ inertia/components/dashboard/DashboardContainer.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import DashboardNavigation from '~/components/dashboard/DashboardNavigation.vue'
+</script>
+
+<template>
+  <div class="dashboard-container">
+    <h3>Dashboard</h3>
+    <div class="dashboard-content">
+      <DashboardNavigation />
+      <div class="dashboard-content-container">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.dashboard-container {
+  width: 80%;
+  margin: 30px auto;
+}
+.dashboard-content {
+  display: flex;
+}
+.dashboard-content-container {
+  min-width: 800px;
+  flex-grow: 1;
+}
+</style>
