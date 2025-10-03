fc784a13fab912c638ca63535dd975d68ae9fe53
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-10-03 18:50:21 +0200

    chore: added authentification packages

--- start/kernel.ts
+++ start/kernel.ts
@@ -7,13 +7,18 @@ server.use([
   () => import('#middleware/container_bindings_middleware'),
   () => import('@adonisjs/static/static_middleware'),
   () => import('@adonisjs/vite/vite_middleware'),
-  () => import('@adonisjs/inertia/inertia_middleware')
+  () => import('@adonisjs/inertia/inertia_middleware'),
 ])
 
 router.use([
   () => import('@adonisjs/core/bodyparser_middleware'),
   () => import('@adonisjs/session/session_middleware'),
   () => import('@adonisjs/shield/shield_middleware'),
+  () => import('@adonisjs/auth/initialize_auth_middleware'),
+  () => import('#middleware/silent_auth_middleware'),
 ])
 
-export const middleware = router.named({})
+export const middleware = router.named({
+  guest: () => import('#middleware/guest_middleware'),
+  auth: () => import('#middleware/auth_middleware'),
+})
