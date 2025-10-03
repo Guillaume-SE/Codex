02f120b1e88fc786cac22729502678fcc64e5e79
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-10-03 18:52:25 +0200

    refactor: added logic to find a user when trying to log

--- app/models/user.ts
+++ app/models/user.ts
@@ -1,14 +1,30 @@
+import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
+import { compose } from '@adonisjs/core/helpers'
+import hash from '@adonisjs/core/services/hash'
 import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { DateTime } from 'luxon'
 
-export default class User extends BaseModel {
-  public static table = 'users'
+const AuthFinder = withAuthFinder(() => hash.use('argon'), {
+  uids: ['username', 'email'],
+  passwordColumnName: 'password',
+})
 
+export default class User extends compose(BaseModel, AuthFinder) {
   @column({ isPrimary: true })
   declare id: number
 
   @column()
   declare username: string
 
+  @column()
+  declare email: string
+
   @column({ serializeAs: null })
   declare password: string
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime | null
 }
