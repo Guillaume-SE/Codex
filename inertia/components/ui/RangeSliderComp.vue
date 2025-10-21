558d993dba3e5451cac9e7f23819a60c85c01198
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-10-21 18:15:41 +0200

    refactor: added size options

--- inertia/components/ui/RangeSliderComp.vue
+++ inertia/components/ui/RangeSliderComp.vue
@@ -1,18 +1,24 @@
 <script setup lang="ts">
+import { computed } from 'vue'
 import InputComp from '~/components/ui/InputComp.vue'
 
 const model = defineModel<string>()
 
-defineProps<{
+const props = defineProps<{
   min?: string
   max?: string
   step?: string
   labels: string[]
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl'
 }>()
+
+const sizeClass = computed(() => {
+  return props.size ? `range-${props.size}` : 'xs'
+})
 </script>
 
 <template>
-  <InputComp v-model="model" type="range" :min="min" :max="max" :step="step" />
+  <InputComp v-model="model" type="range" :min="min" :max="max" :step="step" :class="sizeClass" />
 
   <div class="mt-2 flex justify-between px-2.5 text-xs">
     <span v-for="_ in labels" :key="_">|</span>
