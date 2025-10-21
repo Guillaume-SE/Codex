68fbbecabd980dc8696888454f8dc2ad47c5fc3e
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-10-21 18:16:27 +0200

    refactor: added select size in options

--- inertia/components/ui/SelectComp.vue
+++ inertia/components/ui/SelectComp.vue
@@ -1,21 +1,39 @@
 <script setup lang="ts">
-const model = defineModel()
+import { computed, useSlots } from 'vue'
 
-defineProps<{
-  options: {
+const model = defineModel()
+const slots = useSlots()
+
+const props = defineProps<{
+  options?: {
     text: string
     value: string | number
   }[]
-  disabledValue?: boolean
-  disabledText?: string
+
+  placeholder?: string
+  placeholderValue?: string | number | null
+  size?: 'xs' | 'sm' | 'md' | 'lg'
 }>()
+
+defineOptions({
+  inheritAttrs: false,
+})
+
+const selectClasses = computed(() => ['select', props.size ? `select-${props.size}` : 'xs'])
 </script>
 
 <template>
-  <select v-model="model">
-    <option v-if="disabledValue" disabled value="">{{ disabledText }}</option>
-    <option v-for="option in options" :key="option.value" :value="option.value">
-      {{ option.text }}
+  <select v-model="model" :class="selectClasses" v-bind="$attrs">
+    <option v-if="placeholder" disabled :value="placeholderValue ?? null">
+      {{ placeholder }}
     </option>
+
+    <template v-if="options && !slots.default">
+      <option v-for="option in options" :key="option.value" :value="option.value">
+        {{ option.text }}
+      </option>
+    </template>
+
+    <slot v-else />
   </select>
 </template>
