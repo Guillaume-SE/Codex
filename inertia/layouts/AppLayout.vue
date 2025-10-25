844a6cbcb170efb50a33ca7aec3e77a05a1d6e52
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-10-25 18:37:32 +0200

    refactor: layout now pass user data to navbar component

--- inertia/layouts/AppLayout.vue
+++ inertia/layouts/AppLayout.vue
@@ -1,19 +1,27 @@
 <script setup lang="ts">
+import type User from '#models/user'
+import AppDrawer from '~/components/AppDrawer.vue'
 import AppNavigation from '~/components/AppNavigation.vue'
 import ToastManager from '~/components/ToastManager.vue'
 
 defineProps<{
   success?: string
   error?: string
+  user?: User
 }>()
 </script>
 
 <template>
-  <header>
-    <AppNavigation />
-  </header>
-  <main>
-    <slot />
-  </main>
-  <ToastManager />
+  <AppDrawer>
+    <header>
+      <AppNavigation :user="user" />
+    </header>
+    <main
+      class="bg-muted/40 flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 p-4 md:gap-8 md:p-10"
+    >
+      <slot />
+    </main>
+    <footer></footer>
+    <ToastManager />
+  </AppDrawer>
 </template>
