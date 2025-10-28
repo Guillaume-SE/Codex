a8f07b2f93d959d1674c21f0d4ae6a663c83465a
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-10-28 12:07:03 +0100

    refactor: made comp smarter

--- inertia/components/ui/ButtonComp.vue
+++ inertia/components/ui/ButtonComp.vue
@@ -1,17 +1,67 @@
 <script setup lang="ts">
-interface IButtonProps {
-  type?: 'button' | 'submit'
-  disabled?: boolean
-}
+import { Link } from '@inertiajs/vue3'
+import { computed } from 'vue'
+import Loader from '~/components/ui/Loader.vue'
 
-withDefaults(defineProps<IButtonProps>(), {
-  type: 'button',
-  disabled: false,
+const props = withDefaults(
+  defineProps<{
+    type?: 'button' | 'submit'
+    href?: string
+    disabled?: boolean
+    loading?: boolean
+    variant?: 'primary' | 'secondary' | 'accent' | 'ghost' | 'link'
+    size?: 'xs' | 'sm' | 'lg'
+    outline?: boolean
+  }>(),
+  {
+    type: 'button',
+    disabled: false,
+  }
+)
+
+defineOptions({
+  inheritAttrs: false,
 })
+
+const buttonClasses = computed(() => {
+  return [
+    'btn',
+    props.variant ? `btn-${props.variant}` : '',
+    props.size ? `btn-${props.size}` : '',
+    { 'btn-outline': props.outline },
+    { 'btn-disabled': props.disabled || props.loading },
+  ]
+})
+
+// usefull since Link as his own way to work
+function handleLinkClick(event: MouseEvent) {
+  if (props.disabled || props.loading) {
+    event.preventDefault()
+  }
+}
 </script>
 
 <template>
-  <button :type="type" :disabled="disabled" class="btn">
-    <slot>Valider</slot>
+  <Link
+    v-if="href"
+    v-bind="$attrs"
+    :href="href"
+    :class="[buttonClasses, $attrs.class]"
+    :aria-disabled="disabled || loading"
+    @click="handleLinkClick"
+  >
+    <Loader v-if="loading" />
+    <slot />
+  </Link>
+
+  <button
+    v-else
+    v-bind="$attrs"
+    :type="type"
+    :class="[buttonClasses, $attrs.class]"
+    :disabled="disabled || loading"
+  >
+    <Loader v-if="loading" />
+    <slot />
   </button>
 </template>
