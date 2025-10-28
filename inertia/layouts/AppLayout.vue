dc500cc60f5f9b9adc89d48f0a088206a1fdf1c8
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-10-28 15:47:30 +0100

    chore: added style to the main

--- inertia/layouts/AppLayout.vue
+++ inertia/layouts/AppLayout.vue
@@ -16,9 +16,7 @@ defineProps<{
     <header>
       <AppNavigation :user="user" />
     </header>
-    <main
-      class="bg-muted/40 flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 p-4 md:gap-8 md:p-10"
-    >
+    <main class="bg-muted/40 flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col">
       <slot />
     </main>
     <footer></footer>
