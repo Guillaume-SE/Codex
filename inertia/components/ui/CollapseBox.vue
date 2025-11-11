d036ae581b07516603719a8d0291ca103e5599fb
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-11-11 12:13:49 +0100

    chore: added reusable collapse box

---
+++ inertia/components/ui/CollapseBox.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{
+  label: string
+}>()
+</script>
+
+<template>
+  <div class="bg-base-100 border-base-300 collapse border">
+    <input type="checkbox" />
+    <div class="collapse-title font-semibold">{{ label }}</div>
+    <div class="collapse-content text-sm">
+      <slot />
+    </div>
+  </div>
+</template>
