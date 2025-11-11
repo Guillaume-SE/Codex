38c2305f47110d10e728cfd747ee8131a5c36bed
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-11-11 12:08:48 +0100

    refactor: moved to ui folder

---
+++ inertia/components/ui/RatingBox.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useRatingColorClass } from '~/composables/useRatingColorClass'
+
+const props = defineProps<{
+  rating: number | null | string
+}>()
+
+const numericRating = computed(() => {
+  return props.rating === null ? null : Number(props.rating)
+})
+const ratingColor = computed(() => useRatingColorClass(numericRating.value))
+</script>
+
+<template>
+  <div
+    :class="[
+      'sheen-effect bg-neutral flex h-8 w-8 items-center justify-center rounded-full font-medium',
+      ratingColor,
+    ]"
+  >
+    <span class="text-neutral-content text-lg">{{ numericRating ?? '-' }}</span>
+  </div>
+</template>
---
+++ inertia/components/ui/StatusProgressBadge.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import type { MediaStatuses } from '#types/MediaStatuses'
+import { type PropType, toRef } from 'vue'
+import StatusDotComp from '~/components/ui/StatusDotComp.vue'
+import { useStatusStyles } from '~/composables/useStatusStyle'
+
+const props = defineProps({
+  status: {
+    type: String as PropType<MediaStatuses>,
+    required: true,
+  },
+})
+
+const { backgroundHex, iconHex } = useStatusStyles(toRef(props, 'status'))
+</script>
+
+<template>
+  <div class="status-progress-container">
+    <StatusDotComp class="status-icon" size="12" :color="iconHex" />
+    <span class="status-progress-text">
+      {{ status }}
+    </span>
+  </div>
+</template>
+
+<style scoped>
+.status-progress-container {
+  display: flex;
+  align-items: center;
+  padding: 5px;
+  width: 90px;
+  border-radius: 5px;
+  background-color: v-bind(backgroundHex);
+}
+
+/* .status-progress-container-rounded {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 5px;
+  margin: 10px;
+  width: 15px;
+  height: 15px;
+  border-radius: 50%;
+} */
+
+.status-progress-text {
+  /* color: #fafafa; */
+  color: #0f172a;
+  font-weight: 600;
+  font-size: 0.8rem;
+}
+
+.status-icon {
+  margin-right: 5px;
+}
+</style>
