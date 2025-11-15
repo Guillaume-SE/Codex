159c318497944e16314f32035fe2a1549bd48684
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-11-15 15:12:12 +0100

    fix: fixed issue where 0 and null value was both treated as null

--- inertia/components/media/MediaCard.vue
+++ inertia/components/media/MediaCard.vue
@@ -44,12 +44,12 @@ const favoriteBadgeStatus = computed(() => {
         {{ media.name }}
       </p>
     </Link>
-    <div class="media-card-info-container">
+    <div class="mt-2 flex items-center justify-between">
       <StatusProgressBadge :status="media.status" />
 
       <div class="flex items-center gap-2">
         <FavoriteBadge v-if="favoriteBadgeStatus" :variant="favoriteBadgeStatus" />
-        <RatingBox :rating="media.review?.rating ? media.review.rating : null" />
+        <RatingBox :rating="media.review?.rating ?? null" />
       </div>
     </div>
   </div>
@@ -60,18 +60,5 @@ const favoriteBadgeStatus = computed(() => {
   width: 150px;
   min-height: 290px;
   margin: 10px;
-  /* position: relative; */
-}
-
-/* .icon-favorite {
-  position: absolute;
-  top: -2px;
-  right: 2px;
-  z-index: 3;
-} */
-.media-card-info-container {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
 }
 </style>
