dc2311e4570efd73cee16afb3ec2920dd7012b23
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-12-02 16:02:35 +0100

    refactor: using border to the badge instead of box shadow

--- inertia/components/ui/RatingBox.vue
+++ inertia/components/ui/RatingBox.vue
@@ -15,32 +15,49 @@ const numericRating = computed(() => {
   return isNaN(num) ? null : num
 })
 
+// const ratingColor = computed(() => {
+//   const val = numericRating.value
+
+//   if (val === null) {
+//     return 'rating-default'
+//   }
+
+//   if (val >= 0 && val <= 4) {
+//     return 'rating-bad'
+//   } else if (val >= 5 && val <= 7) {
+//     return 'rating-mid'
+//   } else if (val >= 8 && val <= 10) {
+//     return 'rating-good'
+//   }
+
+//   return 'rating-default'
+// })
 const ratingColor = computed(() => {
   const val = numericRating.value
 
   if (val === null) {
-    return 'rating-default'
+    return 'border-base-300'
   }
 
   if (val >= 0 && val <= 4) {
-    return 'rating-bad'
+    return 'border-error'
   } else if (val >= 5 && val <= 7) {
-    return 'rating-mid'
+    return 'border-warning'
   } else if (val >= 8 && val <= 10) {
-    return 'rating-good'
+    return 'border-success'
   }
 
-  return 'rating-default'
+  return 'border-base-300'
 })
 </script>
 
 <template>
   <div
-    :class="[
-      'bg-neutral flex h-7.5 w-7.5 items-center justify-center rounded-full font-medium',
-      ratingColor,
-    ]"
+    class="bg-neutral flex h-6.5 w-6.5 items-center justify-center rounded-full border-2 font-medium"
+    :class="ratingColor"
   >
-    <span class="text-neutral-content text-lg">{{ numericRating ?? '-' }}</span>
+    <span class="text-neutral-content text-xs font-bold">
+      {{ numericRating ?? '-' }}
+    </span>
   </div>
 </template>
