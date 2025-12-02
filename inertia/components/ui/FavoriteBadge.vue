30f0819ede0beb2585af99055de1b6502ac49053
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-12-02 16:02:00 +0100

    refactor: reduced badge size

--- inertia/components/ui/FavoriteBadge.vue
+++ inertia/components/ui/FavoriteBadge.vue
@@ -15,9 +15,10 @@ withDefaults(
 <template>
   <div
     :class="{ 'sheen-effect': variant === 'filled' }"
-    class="bg-neutral flex h-8 w-8 items-center justify-center rounded-full"
+    class="bg-neutral flex h-5.5 w-5.5 items-center justify-center rounded-full"
+    title="Coup de coeur"
   >
-    <FavoriteIconFilled v-if="variant === 'filled'" class="text-error h-5 w-5" />
-    <FavoriteIconOutline v-if="variant === 'outline'" class="text-neutral-content h-5 w-5" />
+    <FavoriteIconFilled v-if="variant === 'filled'" class="text-error h-4 w-4" />
+    <FavoriteIconOutline v-if="variant === 'outline'" class="text-neutral-content h-4 w-4" />
   </div>
 </template>
