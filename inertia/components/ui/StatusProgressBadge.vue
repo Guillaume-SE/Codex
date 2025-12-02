8bac3a4614e9841d95a3c97e64e0d7b7bcc49126
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-12-02 16:03:05 +0100

    refactor: changed style of the text badge

--- inertia/components/ui/StatusProgressBadge.vue
+++ inertia/components/ui/StatusProgressBadge.vue
@@ -2,6 +2,7 @@
 import type { MediaStatuses } from '#types/MediaStatuses'
 import { computed, type PropType } from 'vue'
 import StatusDotComp from '~/components/ui/StatusDotComp.vue'
+import { useCapitalizeFirstLetter } from '~/composables/useCapitalizeFirstLetter'
 
 const props = defineProps({
   status: {
@@ -26,14 +27,19 @@ const dotColorClass = computed(() => {
       return 'bg-neutral-content'
   }
 })
+
+const formattedStatus = computed(() => {
+  if (!props.status) return ''
+  return useCapitalizeFirstLetter(props.status)
+})
 </script>
 
 <template>
-  <div class="bg-neutral inline-flex items-center gap-1.5 rounded-full px-2.5 py-1">
+  <div class="bg-neutral inline-flex h-full items-center gap-1 rounded-full px-1.5 py-1">
     <StatusDotComp class="h-2 w-2" :color-class="dotColorClass" />
 
-    <span class="text-neutral-content text-xs font-medium">
-      {{ status }}
+    <span class="text-neutral-content p-[1px] text-xs leading-none font-medium">
+      {{ formattedStatus }}
     </span>
   </div>
 </template>
