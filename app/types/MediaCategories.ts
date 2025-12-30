e98bbbb82fccb20dd4fea789df745c511716a386
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2025-12-30 19:34:50 +0100

    refactor: removed unused french type

--- app/types/MediaCategories.ts
+++ app/types/MediaCategories.ts
@@ -1,4 +1,2 @@
 const categories = ['game', 'movie', 'series', 'anime', 'book'] as const
-const categoriesFr = ['jeu', 'film', 'série', 'anime', 'livre'] as const
 export type MediaCategories = (typeof categories)[number]
-export type MediaCategoriesFr = (typeof categoriesFr)[number]
