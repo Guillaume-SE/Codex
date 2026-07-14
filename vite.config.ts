925c7895c35f781dea1510714b1f73ff5d166404
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2026-07-14 16:02:37 +0200

    added packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import adonisjs from '@adonisjs/vite/client'
 import inertia from '@adonisjs/inertia/vite'
+import adonisjs from '@adonisjs/vite/client'
+import tailwindcss from '@tailwindcss/vite'
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [
     vue(),
     inertia({ ssr: { enabled: false, entrypoint: 'inertia/ssr.ts' } }),
     adonisjs({ entrypoints: ['inertia/app.ts'], reload: ['resources/views/**/*.edge'] }),
+    tailwindcss(),
   ],
 
   resolve: {
