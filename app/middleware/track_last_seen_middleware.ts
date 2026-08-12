87b92fd26ac0a4ab925c7d7e4d0de18e54b21bce
Guillaume-SE <100344718+Guillaume-SE@users.noreply.github.com>
2026-08-12 18:04:59 +0200

    refactor: now use logger directly from http context instead of importing

--- app/middleware/track_last_seen_middleware.ts
+++ app/middleware/track_last_seen_middleware.ts
@@ -1,10 +1,9 @@
 import type { HttpContext } from '@adonisjs/core/http'
-import logger from '@adonisjs/core/services/logger'
 import type { NextFn } from '@adonisjs/core/types/http'
 import { DateTime } from 'luxon'
 
 export default class TrackLastSeenMiddleware {
-  async handle({ auth }: HttpContext, next: NextFn) {
+  async handle({ auth, logger }: HttpContext, next: NextFn) {
     const user = auth.user
 
     if (user) {
@@ -12,12 +11,12 @@ export default class TrackLastSeenMiddleware {
 
       if (!user.lastLoggedAt || user.lastLoggedAt < now.minus({ hours: 1 })) {
         user.lastLoggedAt = now
-        await user.save().catch((error) => {
-          logger.error(
-            { err: error, userId: user.id },
-            'Failed to update user lastLoggedAt timestamp'
-          )
-        })
+
+        try {
+          await user.save()
+        } catch (error) {
+          logger.error({ err: error, userId: user.id }, 'Failed to update lastLoggedAt')
+        }
       }
     }
 
